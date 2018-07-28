d22e4e2ab0d5e7cb3129bb8f8808d6729aee81b3
kjabtion <kj@abtion.com>
2018-07-28 10:47:43 +0000

    Worked a lot more on making everything work

--- app/controllers/api_maker/model_controller.rb
+++ app/controllers/api_maker/model_controller.rb
@@ -2,30 +2,32 @@ class ApiMaker::ModelController < ApiMaker::BaseController
   load_and_authorize_resource
 
   def index
-    self.collection_instance = collection_instance.ransack(params[:q]).page(params[:page])
+    ransack = resource_collection.ransack(params[:q])
+    query = ransack.result.page(params[:page])
+    collection = query.map do |model|
+      serialized_resource(model)
+    end
 
-    render json: {
-      collection: collection_instance
-    }
+    render json: {collection: collection}
   end
 
   def show
-    render json: {model: serialized_resource}
+    render json: {model: serialized_resource(resource_instance)}
   end
 
   def new
-    render json: {model: serialized_resource}
+    render json: {model: serialized_resource(resource_instance)}
   end
 
   def create
     if resource_instance.save
       render json: {
-        model: serialized_resource,
+        model: serialized_resource(resource_instance),
         success: true
       }
     else
       render json: {
-        model: serialized_resource,
+        model: serialized_resource(resource_instance),
         success: false,
         errors: resource_instance.errors.full_messages
       }
@@ -33,18 +35,18 @@ class ApiMaker::ModelController < ApiMaker::BaseController
   end
 
   def edit
-    render json: {model: serialized_resource}
+    render json: {model: serialized_resource(resource_instance)}
   end
 
   def update
     if resource_instance.update(sanitize_parameters)
       render json: {
-        model: serialized_resource,
+        model: serialized_resource(resource_instance),
         success: true
       }
     else
       render json: {
-        model: serialized_resource,
+        model: serialized_resource(resource_instance),
         success: false,
         errors: resource_instance.errors.full_messages
       }
@@ -54,12 +56,12 @@ class ApiMaker::ModelController < ApiMaker::BaseController
   def destroy
     if resource_instance.destroy
       render json: {
-        model: serialized_resource,
+        model: serialized_resource(resource_instance),
         success: true
       }
     else
       render json: {
-        model: serialized_resource,
+        model: serialized_resource(resource_instance),
         success: false,
         errors: resource_instance.errors.full_messages
       }
@@ -68,18 +70,33 @@ class ApiMaker::ModelController < ApiMaker::BaseController
 
 private
 
+  def resource_collection
+    @resource_collection ||= proc do
+      variable_name = self.class.name.split("::").last.gsub(/Controller$/, "").parameterize
+      instance_variable_get("@#{variable_name}")
+    end.call
+  end
+
+  def resource_instance_class_name
+    @resource_instance_class_name ||= self.class.name.split("::").last.gsub(/Controller$/, "").singularize
+  end
+
+  def resource_instance_class
+    @resource_instance_class ||= resource_instance_class_name.constantize
+  end
+
   def resource_instance
     @resource_instance ||= proc do
-      variable_name = self.class.name.split("::").last.gsub(/Controller$/, "").singularize.parameterize
+      variable_name = resource_instance_class_name.parameterize
       instance_variable_get("@#{variable_name}")
     end.call
   end
 
   def serializer
-    @serializer ||= ActiveModel::Serializer.get_serializer_for(resource_instance.class)
+    @serializer ||= ActiveModel::Serializer.get_serializer_for(resource_instance_class)
   end
 
-  def serialized_resource
-    @serialized_resource ||= serializer.new(resource_instance)
+  def serialized_resource(model)
+    @serialized_resource ||= serializer.new(model)
   end
 end
--- lib/api_maker.rb
+++ lib/api_maker.rb
@@ -1,6 +1,7 @@
 require "api_maker/engine"
 
 require "active_model_serializers"
+require "ransack"
 require "service_pattern"
 
 module ApiMaker
