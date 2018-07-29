978cf79c9a7f62eacbed12a253e1a586caf8cdeb
kjabtion <kj@abtion.com>
2018-07-29 18:21:39 +0000

    Completed the feature

--- spec/dummy/config/environment.rb
+++ spec/dummy/config/environment.rb
@@ -4,6 +4,4 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-require "cancancan"
 require "webpacker"
-require "will_paginate"
--- spec/features/models/ransack_spec.rb
+++ spec/features/models/ransack_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe "model ransack" do
-  let!(:project_to_find) { create :project, name: "ransack" }
+  let!(:project_to_find) { create :project, name: "test-ransack-kasper" }
   let!(:project_not_to_find) { create :project, name: "kasper" }
 
   it "finds the model", :js do
@@ -9,7 +9,7 @@ describe "model ransack" do
 
     expect(current_path).to eq models_ransack_path
 
-    WaitUtil.wait_for_condition("ransack to complete") { puts page.html; puts chrome_logs; find("[data-controller='models--ransack']", visible: false)["data-ransack-completed"] == "true" }
+    WaitUtil.wait_for_condition("ransack to complete") { find("[data-controller='models--ransack']", visible: false)["data-ransack-completed"] == "true" }
 
     ids = JSON.parse(find("[data-controller='models--ransack']", visible: false).text)
 
