7cac22b6ab9e98630a1c019514a9aca1d81e9a96
kjabtion <kj@abtion.com>
2018-07-30 11:49:49 +0000

    Fixed offence

--- spec/features/devise/current_user_spec.rb
+++ spec/features/devise/current_user_spec.rb
@@ -10,7 +10,9 @@ describe "model belongs to relationships" do
 
     expect(current_path).to eq devise_specs_current_user_spec_path
 
-    WaitUtil.wait_for_condition("user to be found") { find("[data-controller='devise--current-user']", visible: false)["data-current-user-completed"] == "true" }
+    WaitUtil.wait_for_condition("user to be found") do
+      find("[data-controller='devise--current-user']", visible: false)["data-current-user-completed"] == "true"
+    end
 
     project_data = JSON.parse(find("[data-controller='devise--current-user']", visible: false)["data-current-user-result"])
 
