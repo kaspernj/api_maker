42f8f5df32c087459dc279237a4b128d9885195d
kjabtion <39551528+kjabtion@users.noreply.github.com>
2018-08-02 09:54:51 +0200

    Make it possible to read pagination data in result (#21)
    * Make it possible to read pagination data in result
    
    * Fixed complexity offence

--- app/controllers/api_maker/model_controller.rb
+++ app/controllers/api_maker/model_controller.rb
@@ -9,7 +9,10 @@ class ApiMaker::ModelController < ApiMaker::BaseController
 
     collection = ActiveModel::Serializer::CollectionSerializer.new(query)
 
-    render json: {collection: collection}, include: include_param
+    response = {collection: collection}
+    include_pagination_data(response, query)
+
+    render json: response, include: include_param
   end
 
   def show
@@ -80,6 +83,12 @@ private
   def include_param
     return params[:include] if params[:include].present?
     "nothing"
+  end
+
+  def include_pagination_data(response, query)
+    return if params[:page].blank?
+    response[:total_count] = query.total_count
+    response[:total_pages] = query.total_pages
   end
 
   def resource_collection
---
+++ lib/api_maker/javascript/Result.js.erb
@@ -0,0 +1,17 @@
+export default class Collection {
+  constructor(data) {
+    this.data = data
+  }
+
+  models() {
+    return this.data.models
+  }
+
+  totalCount() {
+    return this.data.response.total_count
+  }
+
+  totalPages() {
+    return this.data.response.total_pages
+  }
+}
