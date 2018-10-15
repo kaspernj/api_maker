e21f01d59d2688a0c9917a81a69f54ad9fc4f04b
Kasper Johansen <kaspernj@gmail.com>
2018-10-15 11:58:15 +0200

    Set locale from session variable (#49)

--- app/controllers/api_maker/base_controller.rb
+++ app/controllers/api_maker/base_controller.rb
@@ -1,9 +1,20 @@
 class ApiMaker::BaseController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :set_locale
+
 private
 
   def current_ability
     @current_ability ||= ::ApiMakerAbility.new(controller: self)
+  end
+
+  def set_locale
+    return if session[:locale].blank?
+
+    locale = session[:locale].to_s
+    locale_exists = I18n.available_locales.map(&:to_s).include?(locale)
+
+    I18n.locale = locale if locale_exists
   end
 end
