14ecf151e91b6386444ea3e599dfce71a8ed9b4d
kaspernj <kj@abtion.com>
2018-11-06 12:09:32 +0000

    Fixed offence

--- spec/api_maker/relationship_includer_spec.rb
+++ spec/api_maker/relationship_includer_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 describe ApiMaker::RelationshipIncluder do
   it "parses strings" do
     result = ApiMaker::RelationshipIncluder.parse(["listing.address", "listing.commodity", "listing.user"])
-    expect(result).to eq("listing" => ["address", "commodity", "user"])
+    expect(result).to eq("listing" => %w[address commodity user])
   end
 end
