98ce60af1a59cb75c7e238959c80129c758480d5
kaspernj <kj@abtion.com>
2018-11-19 07:24:28 +0000

    Fixed various spec errors

--- spec/dummy/app/models/project.rb
+++ spec/dummy/app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-  belongs_to :account
+  belongs_to :account, optional: true
 
   has_many :project_secrets, dependent: :destroy
   has_many :tasks, dependent: :destroy, inverse_of: :project
