1714983b29db28bb9a55c3f2e77d7c696fe2313b
kaspernj <kaspernj@gmail.com>
2018-12-19 14:49:06 +0000

    Worked a lot more on the feature

--- lib/api_maker/preloader_has_many.rb
+++ lib/api_maker/preloader_has_many.rb
@@ -23,15 +23,11 @@ private
       # Group them by subquery to fix Postgres grouping-select issues
       query = @reflection.klass.where(@reflection.klass.primary_key => ids_query)
 
-      if @reflection.is_a?(ActiveRecord::Reflection::ThroughReflection)
-        query = query
-          .select(@reflection.klass.arel_table[Arel.star])
-          .select(@reflection.active_record.arel_table[@reflection.active_record.primary_key].as("api_maker_origin_id"))
-      else
-        query = query
-          .select(@reflection.klass.arel_table[Arel.star])
-          .select(@reflection.klass.arel_table[@reflection.foreign_key].as("api_maker_origin_id"))
-      end
+      query = query
+        .distinct
+        .joins(@reflection.inverse_of.name)
+        .select(@reflection.klass.arel_table[Arel.star])
+        .select(@reflection.active_record.arel_table[@reflection.active_record.primary_key].as("api_maker_origin_id"))
 
       query
     end.call
@@ -42,7 +38,8 @@ private
       if @reflection.is_a?(ActiveRecord::Reflection::ThroughReflection)
         query = ApiMaker::PreloaderThrough.new(collection: @collection, reflection: @reflection).models_query_through_reflection
       else
-        query = @reflection.klass.where(@reflection.foreign_key => @collection.map(&:id))
+        primary_key_column = @reflection.options[:primary_key]&.to_sym || @collection.primary_key.to_sym
+        query = @reflection.klass.where(@reflection.foreign_key => @collection.map(&primary_key_column))
       end
 
       query = query.accessible_by(@ability) if @ability
@@ -61,12 +58,9 @@ private
   def preload_model(model)
     origin_id = model.attributes.fetch("api_maker_origin_id")
 
-    puts "PluralName: #{plural_name}"
-    puts "OriginID: #{origin_id}"
-
-    origin_data = @records.find { |record| puts "Record: #{record.fetch(:type)}(#{record.fetch(:id)})"; record.fetch(:type) == plural_name && record.fetch(:id) == origin_id }
-
-    puts "OriginData: #{origin_data}"
+    origin_data = @records.find do |record|
+      record.fetch(:type) == plural_name && record.fetch(:id) == origin_id
+    end
 
     origin_data.fetch(:relationships)[@reflection.name] ||= {data: []}
     origin_data.fetch(:relationships)[@reflection.name].fetch(:data) << {
