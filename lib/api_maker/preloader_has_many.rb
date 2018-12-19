f9f9f4ea1995a364d83775f6de0cf10aaddf29cd
Kasper Johansen <kaspernj@gmail.com>
2018-12-19 15:57:08 +0100

    Fixed Postgres grouping issues with sub queries (#86)
    * Fixed Postgres grouping issues with sub queries
    
    * Worked a lot more on the feature

--- lib/api_maker/preloader_has_many.rb
+++ lib/api_maker/preloader_has_many.rb
@@ -20,15 +20,29 @@ private
 
   def models
     @models ||= proc do
+      # Group them by subquery to fix Postgres grouping-select issues
+      query = @reflection.klass.where(@reflection.klass.primary_key => ids_query)
+
+      query = query
+        .distinct
+        .joins(@reflection.inverse_of.name)
+        .select(@reflection.klass.arel_table[Arel.star])
+        .select(@reflection.active_record.arel_table[@reflection.active_record.primary_key].as("api_maker_origin_id"))
+
+      query
+    end.call
+  end
+
+  def ids_query
+    @ids_query ||= proc do
       if @reflection.is_a?(ActiveRecord::Reflection::ThroughReflection)
         query = ApiMaker::PreloaderThrough.new(collection: @collection, reflection: @reflection).models_query_through_reflection
       else
-        query = @reflection.klass.where(@reflection.foreign_key => @collection.map(&:id))
-        query = query.select(@reflection.klass.arel_table[Arel.star]).select(@reflection.klass.arel_table[@reflection.foreign_key].as("api_maker_origin_id"))
+        primary_key_column = @reflection.options[:primary_key]&.to_sym || @collection.primary_key.to_sym
+        query = @reflection.klass.where(@reflection.foreign_key => @collection.map(&primary_key_column))
       end
 
       query = query.accessible_by(@ability) if @ability
-      query = query.group(@reflection.klass.arel_table[@reflection.klass.primary_key]).fix # Group by ID
       query
     end.call
   end
@@ -44,7 +58,10 @@ private
   def preload_model(model)
     origin_id = model.attributes.fetch("api_maker_origin_id")
 
-    origin_data = @records.find { |record| record.fetch(:type) == plural_name && record.fetch(:id) == origin_id }
+    origin_data = @records.find do |record|
+      record.fetch(:type) == plural_name && record.fetch(:id) == origin_id
+    end
+
     origin_data.fetch(:relationships)[@reflection.name] ||= {data: []}
     origin_data.fetch(:relationships)[@reflection.name].fetch(:data) << {
       type: @reflection.klass.model_name.plural,
