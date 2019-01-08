27fb4c2672916e7e8b935bccf66d4e2c96bbd7a0
Kasper Johansen <kaspernj@gmail.com>
2019-01-08 11:22:15 +0100

    Fixed accessible by and added specs (#99)
    * Fixed accessible by and added specs
    
    * Prepared again

---
+++ spec/features/models/accessible_by_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe "model accessible by" do
+  let!(:project) { create :project }
+  let!(:task) { create :task, id: 3, project: project, user: user }
+  let!(:another_task) { create :task, project: project, user: user }
+  let!(:user) { create :user }
+
+  it "finds the parent model", :js do
+    login_as user
+
+    visit models_accessible_by_path
+
+    expect(current_path).to eq models_accessible_by_path
+
+    wait_for_chrome { find("[data-controller='models--accessible-by']", visible: false)["data-accessible-by-completed"] == "true" }
+
+    project_data = JSON.parse(find("[data-controller='models--accessible-by']", visible: false)["data-result"])
+
+    expect(project_data).to eq [{"id" => task.id, "name" => task.name}]
+  end
+end
