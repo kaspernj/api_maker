588274435fe6891994aae0be83f745ceca0f3947
kaspernj <kj@abtion.com>
2019-04-05 20:11:40 +0200

    Fixed offence

--- lib/api_maker/collection_serializer.rb
+++ lib/api_maker/collection_serializer.rb
@@ -20,7 +20,7 @@ class ApiMaker::CollectionSerializer
         end
       end
 
-      preload_collection(data) if @collection.length > 0
+      preload_collection(data) if @collection.positive?
 
       data
     end
