e270af89d6a7ea5bf439f2805ba0a63d8dd8f822
Kasper Johansen <kaspernj@gmail.com>
2019-04-27 09:36:58 +0200

    Fixed Rails model reloading issues (#143)
    * Fixed Rails model reloading issues
    
    * Fixed disabling of cop

--- lib/api_maker/memory_storage.rb
+++ lib/api_maker/memory_storage.rb
@@ -1,13 +1,11 @@
 class ApiMaker::MemoryStorage
-  attr_reader :resources, :storage
+  attr_reader :storage
 
   def self.current
     @current ||= ApiMaker::MemoryStorage.new
   end
 
   def initialize
-    @resources = []
-    @resources_by_model = {}
     @storage = {}
   end
 
@@ -21,18 +19,7 @@ class ApiMaker::MemoryStorage
     @storage[klass][mode][data] = {data: data, args: args} unless @storage[klass][mode].key?(data)
   end
 
-  def add_resource(klass)
-    return if klass.name == "Resources::ApplicationResource"
-    @resources << {klass: klass}
-  end
-
   def resource_for_model(model_class)
-    resource = @resources_by_model[model_class] ||= @resources
-      .detect { |klass_data| klass_data.fetch(:klass).model_class.name == model_class.name }
-      &.fetch(:klass)
-
-    raise NameError, "Couldnt find resource for model: #{model_class.name}" unless resource
-
-    resource
+    "Resources::#{model_class.name}Resource".safe_constantize
   end
 end
