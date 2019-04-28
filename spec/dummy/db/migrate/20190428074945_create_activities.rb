0f13efa7211f70b24aca77649b8757f3ccbde205
kaspernj <kaspernj@gmail.com>
2019-04-28 08:05:41 +0000

    Fixed offences

--- spec/dummy/db/migrate/20190428074945_create_activities.rb
+++ spec/dummy/db/migrate/20190428074945_create_activities.rb
@@ -1,13 +1,12 @@
 # Migration responsible for creating a table with activities
 class CreateActivities < ActiveRecord::Migration[5.2]
-  # Create table
-  def self.up
+  def up
     create_table :activities do |t|
-      t.belongs_to :trackable, :polymorphic => true
-      t.belongs_to :owner, :polymorphic => true
-      t.string  :key
-      t.text    :parameters
-      t.belongs_to :recipient, :polymorphic => true
+      t.belongs_to :trackable, polymorphic: true
+      t.belongs_to :owner, polymorphic: true
+      t.string :key
+      t.text :parameters
+      t.belongs_to :recipient, polymorphic: true
 
       t.timestamps
     end
@@ -16,8 +15,8 @@ class CreateActivities < ActiveRecord::Migration[5.2]
     add_index :activities, [:owner_id, :owner_type]
     add_index :activities, [:recipient_id, :recipient_type]
   end
-  # Drop table
-  def self.down
+
+  def down
     drop_table :activities
   end
 end
