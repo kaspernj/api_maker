c1e6e5cb87792af9e24d7fe45970e00eca55a217
kaspernj <kaspernj@gmail.com>
2019-05-05 13:32:56 +0000

    Event destroyed component

--- app/services/api_maker/models_generator_service.rb
+++ app/services/api_maker/models_generator_service.rb
@@ -37,7 +37,7 @@ private
   def copy_base_model
     files = %w[
       api.js base-model.js cable-connection-pool.js cable-subscription.js cable-subscription-pool.js collection.js
-      commands-pool.js devise.js event-emitter-listener.jsx event-listener.jsx error-logger.js form-data-to-object.js
+      commands-pool.js devise.js event-destroyed.jsx event-emitter-listener.jsx event-listener.jsx error-logger.js form-data-to-object.js
       included.js logger.js model-name.js models-response-reader.js result.js event-connection.jsx paginate.jsx sort-link.jsx
       updated-attribute.jsx resource-routes.jsx resource-route.jsx session-status-updater.js
       bootstrap/attribute-row.jsx bootstrap/attribute-rows.jsx bootstrap/card.jsx bootstrap/checkbox.jsx bootstrap/checkboxes.jsx
---
+++ lib/api_maker/javascript/event-destroyed.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default class ApiMakerEventDestroyed extends React.Component {
+  static propTypes = {
+    model: PropTypes.object.isRequired,
+    onCall: PropTypes.func.isRequired
+  }
+
+  componentWillMount() {
+    this.connect()
+  }
+
+  componentWillUnmount() {
+    this.connectDestroyed.unsubscribe()
+  }
+
+  connect() {
+    this.connectDestroyed = this.props.model.connectDestroyed(this.props.onCall)
+  }
+
+  render() {
+    return ""
+  }
+}
