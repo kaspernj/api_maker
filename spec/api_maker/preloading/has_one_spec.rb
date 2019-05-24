d3fe1fecc6f1a94c6574211717e4bacea92cc8f5
kjcommodi <50102076+kjcommodi@users.noreply.github.com>
2019-05-24 14:11:53 +0200

    Preloading: Fixed has one through with source crash (#156)
    * Preloading: Fixed has one through with source crash
    
    * Fixed a lot of reflection naming issues

--- spec/api_maker/preloading/has_one_spec.rb
+++ spec/api_maker/preloading/has_one_spec.rb
@@ -1,8 +1,11 @@
 require "rails_helper"
 
 describe "preloading - has one" do
-  let(:project) { create :project }
+  let(:account) { create :account, customer: customer }
+  let(:customer) { create :customer, id: 8 }
+  let(:project) { create :project, account: account }
   let(:project_detail) { create :project_detail, project: project }
+  let(:task) { create :task, id: 6, project: project }
 
   it "sets the relationship to nil and doesnt crash when its preloaded but doesnt exist" do
     collection = Project.where(id: [project.id])
@@ -16,5 +19,12 @@ describe "preloading - has one" do
     result = JSON.parse(ApiMaker::CollectionSerializer.new(collection: collection, include_param: ["project_detail.accounts"]).to_json)
 
     expect(result).to eq("data" => {}, "included" => {})
+  end
+
+  it "loads relationships through and with source" do
+    collection = Task.where(id: task.id)
+    result = JSON.parse(ApiMaker::CollectionSerializer.new(collection: collection, include_param: ["account_customer"]).to_json)
+
+    expect(result.fetch("included").fetch("tasks").fetch("6").fetch("relationships")).to eq("account_customer" => 8)
   end
 end
