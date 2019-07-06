09446b7e8d2a4f507101a7b4924972579e93450b
Kasper Johansen <kaspernj@gmail.com>
2019-07-06 13:21:05 +0200

    Fixed loaded all components issue (#177)

--- lib/api_maker/javascript/resource-route.jsx
+++ lib/api_maker/javascript/resource-route.jsx
@@ -1,12 +1,15 @@
 const inflection = require("inflection")
 
 export default class ResourceRoute {
-  constructor(props) {
-    this.props = props
+  constructor(args) {
+    console.log({ args })
+    this.args = args.args
+    this.parsedContext = args.parsedContext
+    this.route = args.route
   }
 
   routes() {
-    if (this.props.locales) {
+    if (this.args.locales) {
       return this.withLocale()
     } else {
       return this.withoutLocale()
@@ -25,15 +28,26 @@ export default class ResourceRoute {
     return result
   }
 
+  requireComponent() {
+    var requireResult = this.parsedContext[`components/${this.route.component}.jsx`]
+
+    if (!requireResult)
+      var requireResult = this.parsedContext[`components/${this.route.component}/index.jsx`]
+
+    if (!requireResult)
+      throw new Error(`No such require: ${this.route.component}`)
+
+    return requireResult.default
+  }
+
   withLocale() {
-    var { route } = this.props
-    var component = require(`components/${route.component}`).default
+    var component = this.requireComponent()
     var Locales = require("shared/locales").default
     var Path = require("shared/path").default
     var routes = []
 
     for(var locale of Locales.availableLocales()) {
-      var path = Path.localized(inflection.camelize(route.name, true), this.findRouteParams(route), {locale: locale})
+      var path = Path.localized(inflection.camelize(this.route.name, true), this.findRouteParams(this.route), {locale: locale})
 
       routes.push({
         path: path,
@@ -45,15 +59,14 @@ export default class ResourceRoute {
   }
 
   withoutLocale() {
-    var { route } = this.props
-    var routePathName = inflection.camelize(route.name, true)
+    var routePathName = inflection.camelize(this.route.name, true)
     var routePathMethod = Routes[`${routePathName}Path`]
 
     if (!routePathMethod)
       throw new Error(`No such route could be found: ${routePathName}`)
 
-    var path = routePathMethod.apply(null, this.findRouteParams(route))
-    var component = require(`components/${route.component}`).default
+    var path = routePathMethod.apply(null, this.findRouteParams(this.route))
+    var component = this.requireComponent()
 
     return [{
       path: path,
