7c58eac207fdea1f482b138ac851416a7fab1a46
kjabtion <kj@abtion.com>
2019-09-18 10:45:44 +0200

    Finished everything up (I believe)

--- lib/api_maker/javascript/bootstrap/checkboxes.jsx
+++ lib/api_maker/javascript/bootstrap/checkboxes.jsx
@@ -4,6 +4,14 @@ import React from "react"
 const inflection = require("inflection")
 
 export default class BootstrapRadioButtons extends React.Component {
+  static propTypes = PropTypesExact({
+    defaultValue: PropTypes.array,
+    label: PropTypes.string,
+    model: PropTypes.object,
+    name: PropTypes.string,
+    options: PropTypes.array.isRequired
+  })
+
   render() {
     return (
       <div className="component-bootstrap-checkboxes form-group">
@@ -11,7 +19,8 @@ export default class BootstrapRadioButtons extends React.Component {
           {this.label()}
         </label>
 
-        {this.props.options.map(option => this.optionElement(option))}
+        <input name={this.inputName()} type="hidden" value="" />
+        {this.props.options.map((option, index) => this.optionElement(option, index))}
       </div>
     )
   }
@@ -27,7 +36,7 @@ export default class BootstrapRadioButtons extends React.Component {
     }
   }
 
-  inputName(value) {
+  inputName() {
     if (this.props.name) {
       return `${this.props.name}[]`
     } else if (this.props.model) {
@@ -80,11 +89,4 @@ export default class BootstrapRadioButtons extends React.Component {
       </div>
     )
   }
-}
-
-BootstrapRadioButtons.propTypes = {
-  defaultValue: PropTypes.array,
-  model: PropTypes.object,
-  name: PropTypes.string,
-  options: PropTypes.array.isRequired
 }
--- spec/dummy/app/javascript/components/bootstrap/checkboxes.jsx
+++ spec/dummy/app/javascript/components/bootstrap/checkboxes.jsx
@@ -1,32 +1,48 @@
 export default class BootstrapCheckboxes extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {}
   }
 
   componentDidMount() {
     this.loadAccount()
+    this.loadTasks()
   }
 
   async loadAccount() {
     var params = Params.parse()
-    var account = Account.ransack({id_eq: params.account_id}).first()
+    var account = await Account.ransack({id_eq: params.account_id}).preload("tasks").first()
     this.setState({account})
+  }
+
+  async loadTasks() {
+    var tasks = await Task.ransack().toArray()
+    this.setState({tasks})
   }
 
   render() {
     return (
       <Layout>
-        {this.state.account && this.content()}
+        {this.state.account && this.state.tasks && this.content()}
       </Layout>
     )
   }
 
   content() {
+    console.log(this.state.account)
+
     return (
-      <form onSubmit={(e) => this.onSubmit(e)} ref="form">
-        <Checkboxes model={this.state.account} />
-        <input type="submit" value="Save" />
-      </form>
+      <div className="content-container">
+        <form onSubmit={(e) => this.onSubmit(e)} ref="form">
+          <Checkboxes
+            defaultValue={this.state.account.tasks().loaded().map(task => task.id())}
+            label="Choose tasks"
+            name="account[task_ids]"
+            options={this.state.tasks.map(task => [task.name(), task.id()])}
+            />
+          <input type="submit" value="Save" />
+        </form>
+      </div>
     )
   }
 
