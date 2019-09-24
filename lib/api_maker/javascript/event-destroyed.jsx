f977588ddc911ff047766b6047a682bec911f1c1
kjabtion <kj@abtion.com>
2019-09-24 15:30:28 +0200

    Added prop types exact

--- lib/api_maker/javascript/event-destroyed.jsx
+++ lib/api_maker/javascript/event-destroyed.jsx
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types"
+import PropTypesExact from "prop-types-exact"
 import React from "react"
 
 export default class ApiMakerEventDestroyed extends React.Component {
-  static propTypes = {
+  static propTypes = PropTypesExact({
     model: PropTypes.object.isRequired,
     onDestroyed: PropTypes.func.isRequired
-  }
+  })
 
   componentDidMount() {
     this.connect()
