12ec60dc3f5c0953e78c2711b93409e384ff2f6f
kjabtion <kj@abtion.com>
2019-09-27 23:10:09 +0200

    Worked a lot more on the feature

--- app/channels/api_maker/subscriptions_channel.rb
+++ app/channels/api_maker/subscriptions_channel.rb
@@ -23,7 +23,6 @@ private
 
   def connect_creates(model_name)
     model_class = model_name.safe_constantize
-
     channel_name = "api_maker_creates_#{model_class}"
     stream_from(channel_name, coder: ActiveSupport::JSON) do |data|
       transmit data
@@ -34,7 +33,6 @@ private
     ability_name = "event_#{event_name}".to_sym
     model_class = model_name.safe_constantize
     models = model_class.accessible_by(current_ability, ability_name).where(model_class.primary_key => model_ids)
-
     models.each do |model|
       channel_name = "api_maker_events_#{model_class}_#{model.id}_#{event_name}"
       stream_from(channel_name, coder: ActiveSupport::JSON) do |data|
@@ -46,7 +44,6 @@ private
   def connect_updates(model_name, model_ids)
     model_class = model_name.safe_constantize
     models = model_class.accessible_by(current_ability, :update_events).where(model_class.primary_key => model_ids)
-
     models.each do |model|
       channel_name = "api_maker_updates_#{model_class}_#{model.id}"
       stream_from(channel_name, coder: ActiveSupport::JSON) do |data|
@@ -58,9 +55,9 @@ private
   def connect_destroys(model_name, model_ids)
     model_class = model_name.safe_constantize
     models = model_class.accessible_by(current_ability, :update_events).where(model_class.primary_key => model_ids)
-
     models.each do |model|
       channel_name = "api_maker_destroys_#{model_class}_#{model.id}"
+
       stream_from(channel_name, coder: ActiveSupport::JSON) do |data|
         transmit data
       end
--- spec/dummy/app/javascript/api-maker/event-updated.jsx
+++ spec/dummy/app/javascript/api-maker/event-updated.jsx
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types"
+import PropTypesExact from "prop-types-exact"
 import React from "react"
 
 export default class ApiMakerEventUpdated extends React.Component {
-  static propTypes = {
+  static propTypes = PropTypesExact({
     model: PropTypes.object.isRequired,
     onUpdated: PropTypes.func.isRequired
-  }
+  })
 
   componentDidMount() {
     this.connect()
