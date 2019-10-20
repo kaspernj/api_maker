1ce49771e89be714cb289b6c20f396515d899d4d
kaspernj <kaspernj@gmail.com>
2019-10-20 12:04:17 +0000

    Fixed factories to use procs

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :project do
     sequence(:name) { |n| "Project #{n}" }
-    price_per_hour Money.new(10_000, "USD")
+    price_per_hour { Money.new(10_000, "USD") }
   end
 end
