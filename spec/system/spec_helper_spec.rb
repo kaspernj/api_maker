3e3bcaf11fa06998698edb7e14d465a4a95652c9
kaspernj <kaspernj@gmail.com>
2019-11-13 08:58:29 +0000

    Dont describe a class

--- spec/system/spec_helper_spec.rb
+++ spec/system/spec_helper_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe ApiMaker::SpecHelper do
+describe "spec helper" do
   describe "#reset_indexeddb" do
     it "resets the indexed db without failing" do
       visit root_path
