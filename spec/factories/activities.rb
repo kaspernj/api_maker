c586e5690065f5fcc7e357a337984a4f10162a18
kaspernj <kaspernj@gmail.com>
2019-12-16 11:03:15 +0100

    Fixed class -> string offence

--- spec/factories/activities.rb
+++ spec/factories/activities.rb
@@ -1,7 +1,7 @@
 require "public_activity"
 
 FactoryBot.define do
-  factory :activity, class: PublicActivity::Activity do
+  factory :activity, class: "PublicActivity::Activity" do
     association :trackable, factory: :task
   end
 end
