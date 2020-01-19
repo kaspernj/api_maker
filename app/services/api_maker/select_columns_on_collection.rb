49d406f59d17679bd75a5ba5cf56684a84cda73f
kaspernj <kaspernj@gmail.com>
2020-01-19 17:35:34 +0100

    Escape selected columns in SQL and validate that given columns are actually columns to avoid injections

--- app/services/api_maker/select_columns_on_collection.rb
+++ app/services/api_maker/select_columns_on_collection.rb
@@ -10,15 +10,22 @@ class ApiMaker::SelectColumnsOnCollection < ApiMaker::ApplicationService
     new_collection = @collection
     param_name = @model_class.model_name.param_key.dasherize
     selected_columns = @select_columns&.dig(param_name)
+    arel_table = Arel::Table.new(@table_name)
 
     if selected_columns
       selected_columns.each do |column_name|
-        new_collection = new_collection.select("#{@table_name}.#{column_name}")
+        validate_column_name!(column_name)
+        new_collection = new_collection.select(arel_table[column_name])
       end
     else
       new_collection = new_collection.select("#{@table_name}.*")
     end
 
     succeed! new_collection
+  end
+
+  # Checks that the given column exists to avoid injections
+  def validate_column_name!(column_name)
+    raise "Invalid column on #{@model_class.name}: #{column_name}" unless @model_class.columns_hash.key?(column_name)
   end
 end
