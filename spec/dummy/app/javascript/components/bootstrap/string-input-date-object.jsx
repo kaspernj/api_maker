617d7e43593d6865505b5737bc786d6835ea80af
Anders Mikkelsen <anders@agmweb.dk>
2020-02-11 15:56:23 +0100

    fixed issue

--- spec/dummy/app/javascript/components/bootstrap/string-input-date-object.jsx
+++ spec/dummy/app/javascript/components/bootstrap/string-input-date-object.jsx
@@ -1,26 +1,13 @@
-export default class BootstrapStringInputDatetimeLocal extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      user: Devise.currentUser()
-    }
-  }
-
+export default class BootstrapStringInputDateObject extends React.Component {
   render() {
     return (
       <Layout>
-        {this.content()}
+        <div className="content-container">
+          <form onSubmit={(e) => this.onSubmit(e)}>
+            <StringInput defaultValue={new Date(2020, 1, )} id="date_object" label="Birthday" type="date" />
+          </form>
+        </div>
       </Layout>
-    )
-  }
-
-  content() {
-    return (
-      <div className="content-container">
-        <form onSubmit={(e) => this.onSubmit(e)}>
-          <StringInput defaultValue={new Date(2020, 1, )} id="date_object" label="Birthday" type="date" />
-        </form>
-      </div>
     )
   }
 }
