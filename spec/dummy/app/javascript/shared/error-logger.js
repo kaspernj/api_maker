2bab857c404bfae8df2e86be4008da97d32d85bf
kaspernj <kaspernj@gmail.com>
2020-03-04 15:26:11 +0100

    Source maps are loaded dynamically now

--- spec/dummy/app/javascript/shared/error-logger.js
+++ spec/dummy/app/javascript/shared/error-logger.js
@@ -1,6 +1,4 @@
 import ErrorLogger from "api-maker/error-logger"
 
 window.errorLogger = new ErrorLogger()
-window.errorLogger.loadSourceMaps().then(() => {
-  window.errorLogger.enable()
-})
+window.errorLogger.enable()
