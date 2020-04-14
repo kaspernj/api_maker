31a5c5a0da39cf0ac5993aa2998a55f3d5bc86e4
kaspernj <kaspernj@gmail.com>
2020-04-14 10:27:15 +0200

    AIP destroy: Dependent restrict error messages

--- app/services/api_maker/destroy_command.rb
+++ app/services/api_maker/destroy_command.rb
@@ -16,11 +16,16 @@ class ApiMaker::DestroyCommand < ApiMaker::BaseCommand
     end
   end
 
+  def errors_for_model
+    ActiveRecordBetterDependentErrorMessages::DestroyValidator.(model: model) if model.errors.full_messages.empty?
+    model.errors.full_messages
+  end
+
   def failure_response
     command.fail(
       model: serialized_model(model),
       success: false,
-      errors: model.errors.full_messages
+      errors: errors_for_model
     )
   end
 
