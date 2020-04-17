b2454d5e18f9488cc15f8dcb9697d71e7baeee55
Kasper Johansen <kaspernj@gmail.com>
2020-04-17 06:14:26 +0200

    Fixed Bootstrap input money crash (#315)
    * Fixed Bootstrap input money crash
    
    * Upgraded API maker inputs version

---
+++ spec/dummy/app/javascript/routes/bootstrap/string-input-money.jsx
@@ -0,0 +1,60 @@
+import { digg } from "@kaspernj/object-digger"
+import React from "react"
+
+export default class BootstrapStringInputDatetimeLocal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currenciesCollection: [["Danish kroner", "DKK"], ["American Dollars", "USD"]]
+    }
+  }
+
+  componentDidMount() {
+    this.loadProject()
+  }
+
+  async loadProject() {
+    const params = Params.parse()
+    console.log(params)
+
+    const project = await Project.find(digg(params, "project_id"))
+
+    this.setState({project})
+  }
+
+  render() {
+    return (
+      <Layout>
+        {this.state.project && this.content()}
+      </Layout>
+    )
+  }
+
+  content() {
+    const { currenciesCollection, project } = this.state
+
+    return (
+      <div className="content-container">
+        <form onSubmit={(e) => this.onSubmit(e)}>
+          <Input
+            attribute="pricePerHour"
+            currenciesCollection={currenciesCollection}
+            label="Hourly price"
+            model={project}
+            type="money"
+          />
+          <input type="submit" value="Save" />
+        </form>
+      </div>
+    )
+  }
+
+  async onSubmit(e) {
+    e.preventDefault()
+
+    const project = digg(this.state, "project")
+
+    await project.saveRaw(e.target)
+    console.log("Project was saved")
+  }
+}
--- spec/system/bootstrap/string_input_spec.rb
+++ spec/system/bootstrap/string_input_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 describe "bootstrap - string input" do
   let(:task) { create :task, created_at: "1985-06-17 10:30:00", user: user }
+  let(:project) { create :project, price_per_hour: Money.new(100_00, "USD") }
   let(:user) { create :user, birthday_at: "1985-06-17" }
 
   it "renders a date field and sets the value correctly" do
@@ -44,5 +45,21 @@ describe "bootstrap - string input" do
     expect(find("#user_image")[:name]).to eq ""
     attach_file "user_image", Rails.root.join("Gemfile")
     expect(find("#user_image")[:name]).to eq "user[image]"
+  end
+
+  it "renders several components for money" do
+    login_as user
+
+    visit bootstrap_string_input_money_path(project_id: project.id)
+    wait_for_selector ".content-container"
+
+    fill_in "project_price_per_hour", with: 500
+    select "American Dollars", from: "project_price_per_hour_currency"
+
+    find("input[type=submit]").click
+
+    wait_for_browser do
+      project.reload.price_per_hour.format == "$500.00"
+    end
   end
 end
