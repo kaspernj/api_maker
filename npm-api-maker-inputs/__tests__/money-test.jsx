c069db6333fbb786cadeb19f2144d1ffc42fdaaa
Anders Mikkelsen <anders@agmweb.dk>
2020-06-09 10:46:57 +0200

    added spec

---
+++ npm-api-maker-inputs/__tests__/money-test.jsx
@@ -0,0 +1,22 @@
+import Adapter from "enzyme-adapter-react-16"
+import Enzyme, { shallow } from "enzyme"
+import Money from "../src/input"
+import React from "react"
+
+Enzyme.configure({adapter: new Adapter()})
+
+test("shows currency options by default", () => {
+  const wrapper = shallow(
+    <Money currenciesCollection={["DKK"]} />
+  )
+
+  expect(wrapper.find('.component-bootstrap-money-input')).toBeTruthy();
+})
+
+test("hides currency options", () => {
+  const wrapper = shallow(
+    <Money currenciesCollection={["DKK"]} showCurrencyOptions={false} />
+  )
+
+  expect(wrapper.find('.component-bootstrap-money-input').exists()).toBeFalsy();
+})
