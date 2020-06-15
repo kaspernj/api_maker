d7b29d02ba6a7786f6090db20b33ff485379bba7
Kasper Johansen <kaspernj@gmail.com>
2020-06-15 13:57:41 +0100

    Fixed select distinct issues (#346)
    * Fixed not using select, distinct or order for count queries
    
    * Prepend select wild card to avoid issues

--- ruby-gem/app/services/api_maker/select_columns_on_collection.rb
+++ ruby-gem/app/services/api_maker/select_columns_on_collection.rb
@@ -22,10 +22,32 @@ class ApiMaker::SelectColumnsOnCollection < ApiMaker::ApplicationService
         new_collection = new_collection.select(arel_table[column_name])
       end
     else
-      new_collection = new_collection.select("#{@table_name}.*")
+      new_collection = prepend_table_wildcard(new_collection) unless table_wildcard_prepended?(new_collection)
     end
 
     succeed! new_collection
+  end
+
+  # Prepends 'table_name.*' to the query. It needs to be pre-pended in case a `COUNT` or another aggregate function has been added to work with `DISTINCT`.
+  def prepend_table_wildcard(query)
+    old_select = query.values[:select] || []
+    old_select = old_select.keep_if { |select_statement| select_statement != select_table_wildcard_sql }
+
+    query = query.except(:select).select(select_table_wildcard_sql)
+
+    old_select.each do |select_statement|
+      query = query.select(select_statement)
+    end
+
+    query
+  end
+
+  def select_table_wildcard_sql
+    @select_table_wildcard_sql ||= "#{@table_name}.*"
+  end
+
+  def table_wildcard_prepended?(query)
+    query.values[:select]&.first == select_table_wildcard_sql
   end
 
   # Checks that the given column exists to avoid injections
