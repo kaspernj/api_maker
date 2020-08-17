cf3217a0a4f431d0fceecb04257bc02e2b876c04
kaspernj <kaspernj@gmail.com>
2020-08-17 23:20:37 +0200

    Removed debug output

--- ruby-gem/spec/system/bootstrap/checkboxes_spec.rb
+++ ruby-gem/spec/system/bootstrap/checkboxes_spec.rb
@@ -7,75 +7,47 @@ describe "bootstrap - checkboxes" do
   let(:task2) { create :task, user: user }
   let(:user) { create :user }
 
-  before do
-    ActiveRecord::Base.logger = Logger.new(STDOUT)
-  end
-
-  after do
-    ActiveRecord::Base.logger = nil
-  end
-
   it "adds a new relationships" do
     task1
     task2
 
-    puts "SIGN IN"
     login_as user
 
-    puts "VISIT"
     visit bootstrap_checkboxes_path(account_id: account.id)
 
-    puts "WAIT FOR CONTENT"
     wait_for_selector ".content-container"
 
-    puts "CHECK"
     check task1.name
 
-    puts "SUBMIT"
     wait_for_and_find("input[type=submit]").click
 
-    puts "WAIT FOR SUBMIT"
     wait_for_browser { AccountMarkedTask.where(account: account, task: task1).any? }
 
-    puts "EXPECT"
     expect(AccountMarkedTask.where(account: account, task: task2).any?).to eq false
-
-    puts "DONE"
   end
 
   it "deletes the last relationship" do
     account_market_task1
 
-    puts "LOGIN"
     login_as user
 
-    puts "VISIT"
     visit bootstrap_checkboxes_path(account_id: account.id)
 
-    puts "WAIT FOR CONTENT"
     wait_for_selector ".content-container"
 
-    puts "CHECK"
     checkbox_input = wait_for_and_find("input[type='checkbox']")
     expect(checkbox_input[:checked]).to eq "true"
 
-    puts "UNCHECK"
     uncheck task1.name
 
-    puts "SUBMIT"
     wait_for_and_find("input[type=submit]").click
 
     wait_for_browser do
-      puts "WAIT FOR BROWSER"
       account_market_task1.reload
 
-      puts "FOUND AS NOT EXPECTED"
       false
     rescue ActiveRecord::RecordNotFound
-      puts "NOT FOUND AS EXPECTED"
       true
     end
-
-    puts "DONE"
   end
 end
