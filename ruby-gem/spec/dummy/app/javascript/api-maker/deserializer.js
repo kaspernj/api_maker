679102b06c017630dd3d6ca77066aeef0a28390c
kaspernj <kaspernj@gmail.com>
2020-08-18 14:47:38 +0200

    Avoid using database for generating JS models

--- ruby-gem/spec/dummy/app/javascript/api-maker/deserializer.js
+++ ruby-gem/spec/dummy/app/javascript/api-maker/deserializer.js
@@ -8,9 +8,11 @@ export default class ApiMakerDeserializer {
     if (Array.isArray(object)) {
       return object.map(value => ApiMakerDeserializer.parse(value))
     } else if (object && typeof object == "object") {
-      if (object.api_maker_type == "money") {
-        const cents = object.amount
-        const currency = object.currency
+      if (object.api_maker_type == "date" || object.api_maker_type == "time") {
+        return new Date(digg(object, "value"))
+      } else if (object.api_maker_type == "money") {
+        const cents = digg(object, "amount")
+        const currency = digg(object, "currency")
 
         return Money.fromInteger(cents, currency)
       } else if (object.api_maker_type == "model") {
