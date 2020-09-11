6a7b4a50d2cfc97d0f792141af371ca90f295224
kaspernj <kaspernj@gmail.com>
2020-09-11 09:29:37 +0200

    Try not setting version

--- ruby-gem/spec/rails_helper.rb
+++ ruby-gem/spec/rails_helper.rb
@@ -27,7 +27,7 @@ require "waitutil"
 require "webdrivers"
 require "webpacker"
 
-Webdrivers::Chromedriver.required_version = ENV["CHROME_DRIVER_VERSION"].presence || "84.0.4147.30"
+# Webdrivers::Chromedriver.required_version = ENV["CHROME_DRIVER_VERSION"].presence || "84.0.4147.30"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
