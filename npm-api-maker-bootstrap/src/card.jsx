ee91ff73c1c43fe04c6ae9db32e365f48a11cfcf
kaspernj <kaspernj@gmail.com>
2020-09-14 09:19:14 +0200

    Card: Dont render hidden content by default

--- npm-api-maker-bootstrap/src/card.jsx
+++ npm-api-maker-bootstrap/src/card.jsx
@@ -1,29 +1,28 @@
 import {digs} from "@kaspernj/object-digger"
 import PropTypes from "prop-types"
-import PropTypesExact from "prop-types-exact"
 import React from "react"
 
 export default class ApiMakerBootstrapCard extends React.Component {
   static defaultProps = {
     defaultExpanded: true,
     expandable: false,
+    expandableHide: false,
     responsiveTable: true,
     table: false
   }
 
-  static propTypes = PropTypesExact({
+  static propTypes = {
     className: PropTypes.string,
     children: PropTypes.node,
     controls: PropTypes.node,
     defaultExpanded: PropTypes.bool.isRequired,
     expandable: PropTypes.bool.isRequired,
+    expandableHide: PropTypes.bool.isRequired,
     header: PropTypes.node,
-    onClick: PropTypes.func,
     striped: PropTypes.bool,
-    style: PropTypes.object,
     responsiveTable: PropTypes.bool.isRequired,
     table: PropTypes.bool.isRequired
-  })
+  }
 
   constructor(props) {
     super(props)
@@ -33,12 +32,23 @@ export default class ApiMakerBootstrapCard extends React.Component {
   }
 
   render() {
-    const {children, controls, header, onClick, style, table} = this.props
-    const {expandable} = digs(this.props, "expandable")
+    const {
+      children,
+      className,
+      controls,
+      defaultExpanded,
+      expandable,
+      expandableHide,
+      header,
+      responsiveTable,
+      striped,
+      table,
+      ...restProps
+    } = this.props
     const {expanded} = digs(this.state, "expanded")
 
     return (
-      <div className={this.classNames()} onClick={onClick} ref="card" style={style}>
+      <div className={this.classNames()} ref="card" {...restProps}>
         {(controls || expandable || header) &&
           <div className={`card-header ${!expanded && "border-bottom-0"}`}>
             {header}
@@ -59,14 +69,16 @@ export default class ApiMakerBootstrapCard extends React.Component {
             }
           </div>
         }
-        <div className={this.bodyClassNames()}>
-          {table &&
-            <table className={this.tableClassNames()}>
-              {children}
-            </table>
-          }
-          {!table && children}
-        </div>
+        {(expanded || expandableHide) &&
+          <div className={this.bodyClassNames()}>
+            {table &&
+              <table className={this.tableClassNames()}>
+                {children}
+              </table>
+            }
+            {!table && children}
+          </div>
+        }
       </div>
     )
   }
@@ -81,11 +93,11 @@ export default class ApiMakerBootstrapCard extends React.Component {
   }
 
   bodyClassNames() {
-    const {responsiveTable, table} = digs(this.props, "responsiveTable", "table")
+    const {expandableHide, responsiveTable, table} = digs(this.props, "expandableHide", "responsiveTable", "table")
     const {expanded} = digs(this.state, "expanded")
     const classNames = ["card-body"]
 
-    if (!expanded) {
+    if (!expanded && expandableHide) {
       classNames.push("d-none")
     }
 
