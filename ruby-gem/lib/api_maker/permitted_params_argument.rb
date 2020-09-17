36a3174193dba950394405d46e4359459927fd38
kaspernj <kaspernj@gmail.com>
2020-09-17 10:11:07 +0200

    Permitted params argument: Always supply params even if none given

--- ruby-gem/lib/api_maker/permitted_params_argument.rb
+++ ruby-gem/lib/api_maker/permitted_params_argument.rb
@@ -7,6 +7,6 @@ class ApiMaker::PermittedParamsArgument
   end
 
   def params
-    command.args&.dig(:save)
+    @params ||= command.args&.dig(:save) || {}
   end
 end
---
+++ ruby-gem/spec/api_maker/permitted_params_argument_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe ApiMaker::PermittedParamsArgument do
+  describe "#params" do
+    it "returns an empty hash if nothing is given" do
+      command = ApiMaker::IndividualCommand.new(
+        args: nil,
+        collection: nil,
+        command: nil,
+        id: 5,
+        primary_key: 10,
+        response: nil
+      )
+      permitted_params_argument = ApiMaker::PermittedParamsArgument.new(command: command, model: nil)
+
+      expect(permitted_params_argument.params).to eq({})
+    end
+  end
+end
