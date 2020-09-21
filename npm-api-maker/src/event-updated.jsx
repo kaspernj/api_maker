20ba2c8b2b7e87d6c1ef897ed04f38eece17e703
Kasper Stöckel <kaspernj@gmail.com>
2020-09-21 17:29:05 +0200

    Began adding active props to event components (#405)

--- npm-api-maker/src/event-updated.jsx
+++ npm-api-maker/src/event-updated.jsx
@@ -3,7 +3,12 @@ import PropTypesExact from "prop-types-exact"
 import React from "react"
 
 export default class ApiMakerEventUpdated extends React.Component {
+  static defaultProps = {
+    active: true
+  }
+
   static propTypes = PropTypesExact({
+    active: PropTypes.bool.isRequired,
     model: PropTypes.object.isRequired,
     onUpdated: PropTypes.func.isRequired
   })
@@ -17,7 +22,13 @@ export default class ApiMakerEventUpdated extends React.Component {
   }
 
   connect() {
-    this.connectUpdated = this.props.model.connectUpdated(this.props.onUpdated)
+    this.connectUpdated = this.props.model.connectUpdated((...args) => this.onUpdated(...args))
+  }
+
+  onUpdated(...args) {
+    if (this.props.active) {
+      this.props.onUpdated(...args)
+    }
   }
 
   render() {
