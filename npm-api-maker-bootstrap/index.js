58a31e65c1bcc18b66c19ff0f5bf13d1263f7b34
kaspernj <kaspernj@gmail.com>
2020-09-29 18:30:25 +0200

    Worked more on the feature

--- npm-api-maker-bootstrap/index.js
+++ npm-api-maker-bootstrap/index.js
@@ -1,3 +1,16 @@
+import AttributeRow from "./src/attribute-row"
+import AttributeRows from "./src/attribute-rows"
+import Card from "./src/card"
+import Checkbox from "./src/checkbox"
+import Checkboxes from "./src/checkboxes"
+import Input from "./src/input"
+import InvalidFeedback from "./src/invalid-feedback"
+import LiveTable from "./src/live-table"
+import Paginate from "./src/paginate"
+import RadioButtons from "./src/radio-buttons"
+import Select from "./src/select"
+import SortLink from "./src/sort-link"
+
 export {
   AttributeRow,
   AttributeRows,
@@ -11,4 +24,4 @@ export {
   RadioButtons,
   Select,
   SortLink
-} from "./src/index"
+}
--- npm-api-maker/index.js
+++ npm-api-maker/index.js
@@ -1,3 +1,26 @@
+import Api from "./src/api"
+import CanCan from "./src/can-can"
+import CommandSubmitData from "./src/command-submit-data"
+import { CustomError, ValidationError } from "./src/errors"
+import ErrorLogger from "./src/error-logger"
+import EventConnection from "./src/event-connection"
+import EventCreated from "./src/event-created"
+import EventDestroyed from "./src/event-destroyed"
+import EventEmitterListener from "./src/event-emitter-listener"
+import EventListener from "./src/event-listener"
+import EventModelClass from "./src/event-model-class"
+import EventUpdated from "./src/event-updated"
+import FormDataToObject from "./src/form-data-to-object"
+import Logger from "./src/logger"
+import ModelName from "./src/model-name"
+import MoneyFormatter from "./src/money-formatter"
+import Params from "./src/params"
+import Result from "./src/result"
+import Serializer from "./src/serializer"
+import Services from "./src/services"
+import SourceMapsLoader from "./src/source-maps-loader"
+import UpdatedAttribute from "./src/updated-attribute"
+
 export {
   Api,
   CanCan,
@@ -22,4 +45,4 @@ export {
   SourceMapsLoader,
   UpdatedAttribute,
   ValidationError
-} from "./src/index"
+}
