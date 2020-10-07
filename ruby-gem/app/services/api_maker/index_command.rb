49c5a0a840c746c74d37f701b33e4e7ff2f7c407
kaspernj <kaspernj@gmail.com>
2020-10-07 10:23:04 +0200

    Added debug message

--- ruby-gem/app/services/api_maker/index_command.rb
+++ ruby-gem/app/services/api_maker/index_command.rb
@@ -2,6 +2,8 @@ class ApiMaker::IndexCommand < ApiMaker::BaseCommand
   def execute!
     ApiMaker::Configuration.profile("IndexCommand execute") do
       each_command do |command|
+        Rails.logger.debug "API maker: Running index for #{collection.klass.name} with Ransack: #{command.args&.dig(:q)&.permit!&.to_h}"
+
         result = ApiMaker::CollectionLoader.execute!(
           ability: current_ability,
           args: api_maker_args,
