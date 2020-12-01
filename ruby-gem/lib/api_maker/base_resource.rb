73e5edb10c8a9794055e5d5a8436ef2dace4d5e1
kaspernj <kaspernj@gmail.com>
2020-12-01 17:20:44 +0000

    Added more event related events

--- ruby-gem/lib/api_maker/base_resource.rb
+++ ruby-gem/lib/api_maker/base_resource.rb
@@ -3,8 +3,8 @@ class ApiMaker::BaseResource
 
   delegate :can, to: :ability
 
-  CRUD = [:create, :read, :update, :destroy].freeze
-  READ = [:destroy_events, :read, :update_events].freeze
+  CRUD = [:create, :create_events, :read, :update, :update_events, :destroy, :destroy_events].freeze
+  READ = [:create_events, :destroy_events, :read, :update_events].freeze
 
   def self.attributes(*attributes, **args)
     attributes.each do |attribute|
