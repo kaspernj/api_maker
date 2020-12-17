707a2e689c704a49043fc8dec2cb2d1611d39ec0
kaspernj <kaspernj@gmail.com>
2020-12-17 12:49:11 +0000

    Added clone method

--- ruby-gem/lib/api_maker/javascript/base-model.js
+++ ruby-gem/lib/api_maker/javascript/base-model.js
@@ -92,6 +92,16 @@ export default class BaseModel {
     return this.abilities[abilityName]
   }
 
+  clone() {
+    const clone = new this.constructor
+
+    clone.abilities = Object.assign({}, this.abilities)
+    clone.modelData = Object.assign({}, this.modelData)
+    clone.relationshipsCache = Object.assign({}, this.relationshipsCache)
+
+    return clone
+  }
+
   connect(eventName, callback) {
     const cableSubscription = CableConnectionPool.current().connectEvent(this.modelClassData().name, this.primaryKey(), eventName, callback)
     return cableSubscription
