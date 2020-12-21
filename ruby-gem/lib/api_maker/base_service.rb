767ece13503e648b59be941c0a56be2cdda32e94
kaspernj <kaspernj@gmail.com>
2020-12-21 14:40:21 +0000

    Delegate request to controller

--- ruby-gem/lib/api_maker/base_service.rb
+++ ruby-gem/lib/api_maker/base_service.rb
@@ -1,6 +1,8 @@
 class ApiMaker::BaseService < ServicePattern::Service
   attr_reader :args, :api_maker_args, :controller, :current_ability
 
+  delegate :request, allow_nil: true, to: :controller
+
   def initialize(ability: nil, args: {}, api_maker_args: {}, controller: nil)
     @args = args
     @api_maker_args = api_maker_args
