2ec085eec499110adb00105d5288ae14b375c5ed
kaspernj <kaspernj@gmail.com>
2021-01-02 12:40:29 +0100

    Fixed services catch fail! calls

--- ruby-gem/app/services/api_maker/service_command.rb
+++ ruby-gem/app/services/api_maker/service_command.rb
@@ -4,13 +4,12 @@ class ApiMaker::ServiceCommand < ApiMaker::BaseCommand
     service_constant_name = "Services::#{service_name}"
     service_constant = service_constant_name.constantize
     service_args = args.fetch(:service_args)&.permit!&.to_h
-    service_instance = service_constant.new(
+    response = service_constant.execute(
       ability: current_ability,
       args: service_args,
       api_maker_args: api_maker_args,
       controller: controller
     )
-    response = service_instance.execute
 
     if response.success?
       succeed!(response.result)
