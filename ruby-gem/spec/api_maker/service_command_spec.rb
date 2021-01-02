8c5f2467656563b6761f8e3e3823c1c1250a28d8
kaspernj <kaspernj@gmail.com>
2021-01-02 15:46:27 +0100

    Forward service errors and types

---
+++ ruby-gem/spec/api_maker/service_command_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+describe ApiMaker::ServiceCommand do
+  let(:command_response) { ApiMaker::CommandResponse.new(controller: nil) }
+
+  it "generates errors" do
+    expect_any_instance_of(Services::Devise::SignOut).to receive(:execute) { |service| service.fail!("Hello", type: :test_error) }
+    response = ApiMaker::ServiceCommand
+      .new(
+        ability: nil,
+        api_maker_args: nil,
+        collection: nil,
+        collection_instance: nil,
+        command: ApiMaker::IndividualCommand.new(
+          id: nil,
+          args: {service_args: nil, service_name: "Devise::SignOut"},
+          collection: nil,
+          command: nil,
+          response: command_response
+        ),
+        commands: nil,
+        command_response: command_response,
+        controller: nil
+      )
+      .execute!
+
+    expect(response).to eq(
+      type: :failed,
+      data: {
+        errors: [
+          {
+            message: "Hello",
+            type: :test_error
+          }
+        ]
+      }
+    )
+  end
+end
