23f73fb8420e08c43013409b6d3640c5143f7af6
kaspernj <kaspernj@gmail.com>
2021-01-12 12:05:14 +0100

    Removed debug puts

--- ruby-gem/lib/api_maker/resource_routing.rb
+++ ruby-gem/lib/api_maker/resource_routing.rb
@@ -2,7 +2,6 @@ class ApiMaker::ResourceRouting
   def self.install_resource_routes(rails_routes, layout: "react", routes: nil)
     routes ||= JSON.parse(File.read(Rails.root.join("app/javascript/shared/routes.json")))
     routes.fetch("routes").each do |route|
-      puts "Route: #{route} (layout: #{layout})"
       rails_routes.get route.fetch("path") => "#{layout}#show", as: route.fetch("name").to_sym
     end
   end
