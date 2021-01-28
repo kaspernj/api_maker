e4d9e1b46d59ffb33db515ac68155630c048c131
kaspernj <kaspernj@gmail.com>
2021-01-28 10:50:19 +0100

    Allow new domain

--- ruby-gem/spec/dummy/config/environments/development.rb
+++ ruby-gem/spec/dummy/config/environments/development.rb
@@ -61,5 +61,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "dse.development"
+  config.hosts << "dseadmin.development"
 end
