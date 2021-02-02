6a04574f62e1e16765b594a8115ed96646ab75e8
kaspernj <kaspernj@gmail.com>
2021-02-02 13:51:29 +0100

    Set locale

--- ruby-gem/spec/dummy/app/javascript/shared/i18n.js
+++ ruby-gem/spec/dummy/app/javascript/shared/i18n.js
@@ -1,7 +1,10 @@
-const {I18n} = require("@kaspernj/api-maker")
+const {BaseModel, I18n} = require("@kaspernj/api-maker")
 const i18nInstance = new I18n()
 const ymlFiles = require.context("../../../config/locales", true, /^(.+).yml$/)
 
+i18nInstance.setLocale("en")
 i18nInstance.scanRequireContext(ymlFiles)
+
+BaseModel.setI18n(i18nInstance)
 
 export default i18nInstance
