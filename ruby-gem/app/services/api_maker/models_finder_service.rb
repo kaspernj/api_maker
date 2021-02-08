bd38a82fe62c6034463fa0904a820351689a7342
Anders Mikkelsen <andersgm@outlook.dk>
2021-02-08 19:41:58 +0100

    Unit test considered files

--- ruby-gem/app/services/api_maker/models_finder_service.rb
+++ ruby-gem/app/services/api_maker/models_finder_service.rb
@@ -7,7 +7,7 @@ private
 
   def models
     result = []
-    Dir.glob(Rails.root.join("app/api_maker/resources/**/*_resource.rb")) do |model_path|
+    files.each do |model_path|
       next unless model_path.start_with?(Rails.root.to_s)
 
       path_name = model_path.gsub(/\A#{Regexp.escape(Rails.root.to_s)}\/app\/api_maker\/resources\//, "").gsub(/\.rb\Z/, "")
@@ -18,5 +18,9 @@ private
     end
 
     result
+  end
+
+  def files
+    @files ||= Dir.glob(Rails.root.join("app/api_maker/resources/**/*_resource.rb"))
   end
 end
--- ruby-gem/spec/dummy/app/api_maker/resources/bogus.rb
+++ ruby-gem/spec/dummy/app/api_maker/resources/bogus.rb
@@ -1,6 +1,3 @@
 class Resources::Bogus < Resources::ApplicationResource
   # This file will make sure that only files ending with _resource.rb are considered
-  def initialize
-    raise "Non resource files shouldn't be included"
-  end
 end
