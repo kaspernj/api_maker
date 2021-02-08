d8ab65a659824a05349bf450393b26c80c0a92b5
Anders Mikkelsen <andersgm@outlook.dk>
2021-02-08 19:04:16 +0100

    Added comment

--- ruby-gem/spec/dummy/app/api_maker/resources/bogus.rb
+++ ruby-gem/spec/dummy/app/api_maker/resources/bogus.rb
@@ -1,5 +1,6 @@
 class Resources::Bogus < Resources::ApplicationResource
+  # This file will make sure that only files ending with _resource.rb are considered
   def initialize
-    raise "I Should not be loaded"
+    raise "Non resource files shouldn't be included"
   end
 end
