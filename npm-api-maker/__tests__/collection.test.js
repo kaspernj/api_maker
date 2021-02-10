970c6864a327ec5b307150a9a3551901d916a66b
kaspernj <k@spernj.org>
2021-02-10 14:58:47 +0100

    Merge strings into arrays

--- npm-api-maker/__tests__/collection.test.js
+++ npm-api-maker/__tests__/collection.test.js
@@ -2,16 +2,24 @@ const Collection = require("../src/collection.cjs")
 
 describe("Collection", () => {
   describe("selectColumns", () => {
-    fit("adds selected columns to the query", () => {
+    it("adds selected columns to the query", () => {
       let collection = new Collection({}, {})
 
       collection = collection.selectColumns({User: ["id"]})
-
       expect(collection.queryArgs.selectColumns).toEqual({user: ["id"]})
 
       collection = collection.selectColumns({User: ["email"]})
-
       expect(collection.queryArgs.selectColumns).toEqual({user: ["id", "email"]})
+    })
+
+    it("handles sorts of different types", () => {
+      let collection = new Collection({}, {})
+
+      collection = collection.ransack({s: "created_at"})
+      expect(collection.queryArgs.ransack.s).toEqual("created_at")
+
+      collection = collection.ransack({s: ["id", "name"]})
+      expect(collection.queryArgs.ransack.s).toEqual(["created_at", "id", "name"])
     })
   })
 })
