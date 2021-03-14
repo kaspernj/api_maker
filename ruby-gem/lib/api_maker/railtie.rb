3468a5e69ae51d10a9c2aef66f4ae2484c86f15b
kaspernj <k@spernj.org>
2021-03-14 09:04:09 +0100

    Fixed File.exists? -> File.exist? offence

--- ruby-gem/lib/api_maker/railtie.rb
+++ ruby-gem/lib/api_maker/railtie.rb
@@ -1,6 +1,6 @@
 class ApiMaker::Railtie < Rails::Railtie
   initializer "watch routes.json for changes and reload Rails routes if changed" do |_app|
     file_path = Rails.root.join("app/javascript/shared/routes.json")
-    ApiMaker::RoutesFileReloader.execute!(file_paths: [file_path]) if File.exists?(file_path)
+    ApiMaker::RoutesFileReloader.execute!(file_paths: [file_path]) if File.exist?(file_path)
   end
 end
