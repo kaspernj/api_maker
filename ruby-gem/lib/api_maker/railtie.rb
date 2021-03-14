cbf863241569c117ddc1e9664ddbebe6bac261cf
kaspernj <k@spernj.org>
2021-03-14 09:03:59 +0100

    Fixed unused var offence

--- ruby-gem/lib/api_maker/railtie.rb
+++ ruby-gem/lib/api_maker/railtie.rb
@@ -1,5 +1,5 @@
 class ApiMaker::Railtie < Rails::Railtie
-  initializer "watch routes.json for changes and reload Rails routes if changed" do |app|
+  initializer "watch routes.json for changes and reload Rails routes if changed" do |_app|
     file_path = Rails.root.join("app/javascript/shared/routes.json")
     ApiMaker::RoutesFileReloader.execute!(file_paths: [file_path]) if File.exists?(file_path)
   end
