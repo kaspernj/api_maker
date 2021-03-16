5a9bc809bb855494f050cb0f822b31fdd387fbda
Anders Mikkelsen <andersgm@outlook.dk>
2021-03-16 11:32:05 +0100

    Simplified query and expected that it is present in specs

--- ruby-gem/spec/dummy/app/api_maker/resources/account_marked_task_resource.rb
+++ ruby-gem/spec/dummy/app/api_maker/resources/account_marked_task_resource.rb
@@ -2,7 +2,7 @@ class Resources::AccountMarkedTaskResource < Resources::ApplicationResource
   attributes :id
 
   def abilities
-    can :read,AccountMarkedTask, ["(account_marked_tasks.id = 5)"] do |account_marked_task|
+    can :read, AccountMarkedTask, ["account_marked_tasks.id = 5"] do |account_marked_task|
       account_marked_task.id == 5
     end
   end
