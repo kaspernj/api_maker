f4dc78059d078f1ce24b81b1b0412932d96cfb12
Anders Mikkelsen <andersgm@outlook.dk>
2021-03-16 11:58:17 +0100

    First iteration of belongs to working

--- ruby-gem/lib/api_maker/base_resource.rb
+++ ruby-gem/lib/api_maker/base_resource.rb
@@ -129,6 +129,19 @@ class ApiMaker::BaseResource
           can ability, model_class, [nested_sql] do |model|
             model_class.where(nested_sql).exists?(id: model.id)
           end
+        elsif reflection.macro == :belongs_to
+          relationship_sql = relevant_rule.conditions.first
+          nested_sql = "EXISTS (" \
+            "SELECT 1 " \
+            "FROM #{reflection.klass.table_name} " \
+            "WHERE " \
+              "#{reflection.klass.table_name}.#{reflection.join_primary_key} = #{model_class.table_name}.#{reflection.foreign_key} AND " \
+              "(#{relationship_sql})" \
+          ")"
+
+          can ability, model_class, [nested_sql] do |model|
+            model_class.where(nested_sql).exists?(id: model.id)
+          end
         else
           raise "No support for macro: #{reflection.macro}"
         end
