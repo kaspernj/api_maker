4eae65d728e085b61bfe424aee30a94c46247bd8
kaspernj <k@spernj.org>
2021-05-08 23:11:48 +0200

    Register routes as duplicates as instead

--- ruby-gem/lib/api_maker/resource_routing.rb
+++ ruby-gem/lib/api_maker/resource_routing.rb
@@ -5,12 +5,18 @@ class ApiMaker::ResourceRouting
 
     route_definitions.fetch("routes").each do |route|
       route_name = route.fetch("name").to_sym
+      route_as = route_name
       route_path = route.fetch("path")
 
-      unless installed_routes.key?(route_name)
-        rails_routes.get route_path => "#{layout}#show", as: route_name
-        installed_routes[route_name] = true
+      if installed_routes.key?(route_name)
+        route_duplicate_count = installed_routes.fetch(route_name)
+        route_as = "#{route_as}_duplicate_#{route_duplicate_count}"
       end
+
+      rails_routes.get route_path => "#{layout}#show", as: route_as
+
+      installed_routes[route_name] ||= 0
+      installed_routes[route_name] += 1
     end
   end
 end
