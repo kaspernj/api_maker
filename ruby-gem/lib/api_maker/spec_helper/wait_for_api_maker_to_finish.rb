1fa99400a9a868319b77e3c01231de69324c8dd8
kaspernj <k@spernj.org>
2021-05-13 13:44:47 +0200

    Dont use 'wait_for_browser' because it shouldn't raise any errors - just ensure no loose requests are still running

--- ruby-gem/lib/api_maker/spec_helper/wait_for_api_maker_to_finish.rb
+++ ruby-gem/lib/api_maker/spec_helper/wait_for_api_maker_to_finish.rb
@@ -6,7 +6,7 @@ module ApiMaker::SpecHelper::WaitForApiMakerToFinish
   end
 
   def wait_for_api_maker_to_finish
-    wait_for_browser do
+    WaitUtil.wait_for_condition("api maker to finish requests", timeout_sec: 5, delay_sec: 0.1) do
       raise "The page was reset so couldn't wait" if page.html.include?("<html><head></head><body></body></html>")
 
       result = execute_script "
