a8caec283f346c8bb5a1501bc49ff33a290383a2
kaspernj <k@spernj.org>
2021-05-17 09:03:31 +0000

    Removed synlinks because it bugs up the packages

--- npm-api-maker-inputs/.eslintrc.js
+++ npm-api-maker-inputs/.eslintrc.js
@@ -1,1 +1,46 @@
-../npm-api-maker/.eslintrc.js
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jest/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "jest",
+    "react"
+  ],
+  "settings": {
+    "react": {
+      "version": "16.13.1"
+    }
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ]
+  }
+}
