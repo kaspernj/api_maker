83521f34402d6943cc933a3bbe049f4a6b15fdd3
Anders Mikkelsen <andersgm@outlook.dk>
2021-08-13 16:46:10 +0200

    Added more spec

--- ruby-gem/spec/services/api_maker/deep_merge_params_spec.rb
+++ ruby-gem/spec/services/api_maker/deep_merge_params_spec.rb
@@ -1,10 +1,34 @@
 require "rails_helper"
 
 describe ApiMaker::DeepMergeParams do
-  it "merges arrays" do
+  it "merges hashes in arrays" do
     hash1 = {
       products: [
         {id: 1},
+        {id: 2}
+      ]
+    }
+
+    hash2 = {
+      products: [
+        {name: "Product 1"},
+        {name: "Product 2"},
+      ]
+    }
+
+    expect(ApiMaker::DeepMergeParams.execute!(hash1, hash2)).to eq(
+      {
+        products: [
+          {id: 1, name: "Product 1"},
+          {id: 2, name: "Product 2"}
+        ]
+      }
+    )
+  end
+
+  it "returns the last array if they differ in length" do
+    hash1 = {
+      products: [
         {id: 2}
       ]
     }
@@ -19,7 +43,8 @@ describe ApiMaker::DeepMergeParams do
     expect(ApiMaker::DeepMergeParams.execute!(hash1, hash2)).to eq(
       {
         products: [
-          {id: 1, name: "Product 1"}, {id: 2, name: "Product 2"}
+          {name: "Product 1"},
+          {name: "Product 2"}
         ]
       }
     )
