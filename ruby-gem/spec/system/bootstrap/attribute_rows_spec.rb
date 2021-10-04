d9d8950aaf51b275168bec168594bfbd96ae65f3
kaspernj <k@spernj.org>
2021-10-04 14:07:48 +0000

    Try fixing bot not requiring element to be visible for browser

--- ruby-gem/spec/system/bootstrap/attribute_rows_spec.rb
+++ ruby-gem/spec/system/bootstrap/attribute_rows_spec.rb
@@ -11,9 +11,13 @@ describe "bootstrap attribute rows" do
     visit bootstrap_attribute_rows_path
 
     # Shouldn't crash because 'updated at' isn't loaded
-    wait_for_expect { expect(wait_for_and_find(".user-row[data-user-id='#{user.id}'] .updated-at-column td").text).to eq "" }
+    wait_for_expect do
+      expect(wait_for_and_find(".user-row[data-user-id='#{user.id}'] .updated-at-column td", visible: false).text).to eq ""
+    end
 
     # Should show 'updated at' so it shouldn't be empty
-    wait_for_expect { expect(wait_for_and_find(".user-row[data-user-id='#{user_kasper.id}'] .updated-at-column td").text).not_to eq "" }
+    wait_for_expect do
+      expect(wait_for_and_find(".user-row[data-user-id='#{user_kasper.id}'] .updated-at-column td", visible: false).text).not_to eq ""
+    end
   end
 end
