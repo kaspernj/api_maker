4542e54a03b6dcb28055554b0c8abadeadd8e773
kaspernj <k@spernj.org>
2022-02-10 19:11:17 +0000

    Fixed several more errors and added spec

---
+++ npm-api-maker/__tests__/custom-error.test.js
@@ -0,0 +1,13 @@
+const CustomError = require("../src/custom-error.cjs")
+
+describe("CustomError", () => {
+  it("handles when response is a string", () => {
+    const xhr = {status: 401}
+    const response = "An error happened"
+    const customError = new CustomError(`Request failed with code: ${xhr.status}`, {response, xhr})
+
+    expect(customError.message).toEqual("Request failed with code: 401")
+    expect(customError.errorMessages()).toEqual(undefined)
+    expect(customError.errorTypes()).toEqual(undefined)
+  })
+})
