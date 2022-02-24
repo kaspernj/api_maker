bffde5f834447cca40c0592d01b95de99338caf4
kaspernj <k@spernj.org>
2022-02-24 15:09:32 +0000

    Removed unused import

--- ruby-gem/spec/dummy/app/javascript/shared/error-logger.js
+++ ruby-gem/spec/dummy/app/javascript/shared/error-logger.js
@@ -1,5 +1,4 @@
 import { ErrorLogger } from "@kaspernj/api-maker"
-import { AttributeRow, AttributeRows } from "@kaspernj/api-maker-bootstrap"
 
 global.errorLogger = new ErrorLogger()
 global.errorLogger.enable()
