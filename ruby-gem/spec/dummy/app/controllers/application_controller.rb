a1704f9a2257064c98dea5e6e279c55e6eac9abf
kaspernj <k@spernj.org>
2022-03-16 08:55:20 +0000

    Fixed caching not working with changing user at sign in

--- ruby-gem/spec/dummy/app/controllers/application_controller.rb
+++ ruby-gem/spec/dummy/app/controllers/application_controller.rb
@@ -2,10 +2,12 @@ class ApplicationController < ActionController::Base
 private
 
   def api_maker_args
-    @api_maker_args ||= {current_user: current_user}
+    @api_maker_args ||= {}
+    @api_maker_args[current_user&.id || "anon"] ||= {current_user: current_user}
   end
 
   def current_ability
-    @current_ability ||= ::ApiMaker::Ability.new(api_maker_args: api_maker_args)
+    @current_ability ||= {}
+    @current_ability[current_user&.id || "anon"] ||= ::ApiMaker::Ability.new(api_maker_args: api_maker_args)
   end
 end
