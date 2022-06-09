334eda571cb5ca0e55c86cbb3d4c92162387aca3
kaspernj <k@spernj.org>
2022-06-09 21:30:56 +0000

    Added withCurrentUser wrapper helper

---
+++ npm-api-maker/src/with-current-user.jsx
@@ -0,0 +1,28 @@
+import {digs} from "diggerize"
+import EventEmitterListener from "./event-emitter-listener"
+import PureComponent from "set-state-compare/src/pure-component"
+
+export default (WrappedComponent) => class WithCurrentUser extends PureComponent {
+  state = {
+    currentUser: Devise.currentUser()
+  }
+
+  render() {
+    const {onDeviseSigned} = digs(this, "onDeviseSigned")
+    const {currentUser} = digs(this.state, "currentUser")
+
+    return (
+      <>
+        <EventEmitterListener events={Devise.events()} event="onDeviseSignIn" onCalled={onDeviseSigned} />
+        <EventEmitterListener events={Devise.events()} event="onDeviseSignOut" onCalled={onDeviseSigned} />
+        <WrappedComponent {...this.props} currentUser={currentUser} />
+      </>
+    )
+  }
+
+  onDeviseSigned = () => {
+    const currentUser = Devise.currentUser()
+
+    if (this.state.currentUser !== currentUser) this.setState({currentUser})
+  }
+}
