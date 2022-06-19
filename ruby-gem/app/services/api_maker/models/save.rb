98c3aab944a4558877c9aeb648029374622ac2c5
kaspernj <k@spernj.org>
2022-06-19 20:52:09 +0000

    Validation errors on collection commands

--- ruby-gem/app/services/api_maker/models/save.rb
+++ ruby-gem/app/services/api_maker/models/save.rb
@@ -1,29 +1,66 @@
 class ApiMaker::Models::Save < ApiMaker::ApplicationService
-  attr_reader :models, :simple_model_errors
+  attr_reader :models, :simple_model_errors, :succeed_with_errors
 
-  def initialize(models:, simple_model_errors: false)
+  def initialize(models:, succeed_with_errors: false, simple_model_errors: false)
     @models = models
     @simple_model_errors = simple_model_errors
+    @succeed_with_errors = succeed_with_errors
   end
 
   def perform
     errors = []
+    failed = false
+    failed_models = []
 
-    models.first.transaction do
-      models.each do |model|
+    first_model.transaction do
+      each_model_with_params do |model, params|
+        model.assign_attributes(params)
+
         next if model.save
 
         if simple_model_errors
           errors += ApiMaker::SimpleModelErrors.execute!(model: model)
         else
-          errors += models.map(&:errors).map(&:full_messages).flatten
+          errors += model.errors.full_messages
         end
 
-        raise ActiveRecord::Rollback if errors.any?
+        failed = true
+        failed_models << {model: model, params: params}
+
+        # The other models might be dependent on this one saving and it could lead to actual crashes and not just validation errors
+        break if failed
       end
+
+      raise ActiveRecord::Rollback if errors.any?
     end
 
-    fail! errors.uniq if errors.any?
-    succeed!
+    fail! errors.uniq if errors.any? && !succeed_with_errors
+
+    succeed!(
+      failed: failed,
+      failed_models: failed_models,
+      errors: errors
+    )
+  end
+
+  def each_model_with_params
+    models.each do |model_or_hash|
+      if model_or_hash.is_a?(Hash)
+        params = model_or_hash.fetch(:params)
+        model = model_or_hash.fetch(:model)
+      else
+        model = model_or_hash
+      end
+
+      yield model, params
+    end
+  end
+
+  def first_model
+    each_model_with_params do |model|
+      return model
+    end
+
+    raise "Couldn't find first model"
   end
 end
