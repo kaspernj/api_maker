4485d286f3c95a851818d35d0f9e50a0daad0fa0
kaspernj <k@spernj.org>
2022-07-11 09:19:36 +0000

    Import files directly - no global import/export stuff

--- npm-api-maker-inputs/index.js
+++ npm-api-maker-inputs/index.js
@@ -1,17 +1,1 @@
-import Checkbox from "./src/checkbox"
-import idForComponent from "./src/id-for-component.mjs"
-import Input from "./src/input"
-import Money from "./src/money"
-import nameForComponent from "./src/name-for-component.mjs"
-import Select from "./src/select"
-import inputWrapper from "./src/input-wrapper"
-
-export {
-  Checkbox,
-  idForComponent,
-  Input,
-  Money,
-  nameForComponent,
-  Select,
-  inputWrapper
-}
+export {}
