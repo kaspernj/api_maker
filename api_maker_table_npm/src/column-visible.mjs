bf7530995d91fe2ca7201a150bffd435acf300b0
kaspernj <k@spernj.org>
2022-07-31 20:58:05 +0000

    Hide if no column

--- api_maker_table_npm/src/column-visible.mjs
+++ api_maker_table_npm/src/column-visible.mjs
@@ -1,5 +1,6 @@
 export default function columnVisible(column, tableSettingColumn) {
   if (tableSettingColumn.visible() !== null) return tableSettingColumn.visible()
+  if (!column) return false
   if ("defaultVisible" in column) return column.defaultVisible
 
   return true
