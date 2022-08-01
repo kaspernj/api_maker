3c4e5b599ec54499e9e4bef5d315fe19bf8297b6
kaspernj <k@spernj.org>
2022-08-01 18:16:30 +0000

    loadByQueryParam support

--- npm-api-maker/src/model-load-wrapper.jsx
+++ npm-api-maker/src/model-load-wrapper.jsx
@@ -1,101 +1,116 @@
 import EventUpdated from "./event-updated"
 import Params from "./params.mjs"
+import PropTypes from "prop-types"
 import React from "react"
+import withQueryParams from "on-location-changed/src/with-query-params"
 
-export default (WrappedComponent, ModelClass, args = {}) => class modelLoadWrapper extends React.PureComponent {
-  camelizedLower = ModelClass.modelName().camelizedLower()
-  paramsVariableName = `${ModelClass.modelName().paramKey()}_id`
-
-  state = {
-    model: undefined,
-    modelId: this.getModelId(),
-    notFound: undefined
-  }
-
-  componentDidMount() {
-    this.loadModel()
-  }
-
-  componentDidUpdate() {
-    const newModelId = this.getModelId()
-
-    // The model ID was changed in the URL and a different model should be loaded
-    if (newModelId != this.state.modelId) {
-      this.setState({model: undefined, modelId: newModelId})
-      this.loadExistingModel()
-    }
-  }
-
-  loadModel = async () => {
-    if (args.newIfNoId && !this.getModelId()) {
-      return await this.loadNewModel()
-    } else if (!args.optional || this.getModelId()) {
-      return await this.loadExistingModel()
-    }
-  }
-
-  getModelId() {
-    return this.props.match.params[this.paramsVariableName] || this.props.match.params.id
-  }
-
-  loadExistingModel = async () => {
-    const modelId = this.getModelId()
-    const query = await ModelClass.ransack({id_eq: modelId})
-
-    if (args.abilities) query.abilities(args.abilities)
-    if (args.preload) query.preload(args.preload)
-    if (args.select) query.select(args.select)
-
-    const model = await query.first()
-
-    this.setState({
-      model,
-      notFound: !model
-    })
-  }
-
-  async loadNewModel() {
-    const params = Params.parse()
-    const paramKey = ModelClass.modelName().paramKey()
-    const modelDataFromParams = params[paramKey] || {}
-
-    let defaults = {}
-
-    if (args.newIfNoId?.defaults) {
-      defaults = await args.newIfNoId.defaults()
+export default (WrappedComponent, ModelClass, args = {}) => {
+  class ModelLoadWrapper extends React.PureComponent {
+    static propTypes = {
+      queryParams: PropTypes.object
     }
 
-    const modelData = Object.assign(defaults, args.newAttributes, modelDataFromParams)
-    const model = new ModelClass({
-      isNewRecord: true,
-      data: {a: modelData}
-    })
+    camelizedLower = ModelClass.modelName().camelizedLower()
+    paramsVariableName = `${ModelClass.modelName().paramKey()}_id`
 
-    this.setState({model})
+    state = {
+      model: undefined,
+      modelId: this.getModelId(),
+      notFound: undefined
+    }
+
+    componentDidMount() {
+      this.loadModel()
+    }
+
+    componentDidUpdate() {
+      const newModelId = this.getModelId()
+
+      // The model ID was changed in the URL and a different model should be loaded
+      if (newModelId != this.state.modelId) {
+        this.setState({model: undefined, modelId: newModelId})
+        this.loadExistingModel()
+      }
+    }
+
+    loadModel = async () => {
+      if (args.newIfNoId && !this.getModelId()) {
+        return await this.loadNewModel()
+      } else if (!args.optional || this.getModelId()) {
+        return await this.loadExistingModel()
+      }
+    }
+
+    getModelId() {
+      if (args.loadByQueryParam)
+        return args.loadByQueryParam({props: this.props})
+
+      return this.props.match.params[this.paramsVariableName] || this.props.match.params.id
+    }
+
+    loadExistingModel = async () => {
+      const modelId = this.getModelId()
+      const query = await ModelClass.ransack({id_eq: modelId})
+
+      if (args.abilities) query.abilities(args.abilities)
+      if (args.preload) query.preload(args.preload)
+      if (args.select) query.select(args.select)
+
+      const model = await query.first()
+
+      this.setState({
+        model,
+        notFound: !model
+      })
+    }
+
+    async loadNewModel() {
+      const params = Params.parse()
+      const paramKey = ModelClass.modelName().paramKey()
+      const modelDataFromParams = params[paramKey] || {}
+
+      let defaults = {}
+
+      if (args.newIfNoId?.defaults) {
+        defaults = await args.newIfNoId.defaults()
+      }
+
+      const modelData = Object.assign(defaults, args.newAttributes, modelDataFromParams)
+      const model = new ModelClass({
+        isNewRecord: true,
+        data: {a: modelData}
+      })
+
+      this.setState({model})
+    }
+
+    render() {
+      const {onUpdated, reloadModel} = digs(this, "onUpdated", "reloadModel")
+      const {model, modelId, notFound} = digs(this.state, "model", "modelId", "notFound")
+      const wrappedComponentProps = {}
+
+      wrappedComponentProps[this.camelizedLower] = model
+      wrappedComponentProps[`${this.camelizedLower}Id`] = modelId
+      wrappedComponentProps[`${this.camelizedLower}NotFound`] = notFound
+
+      return (
+        <>
+          {args.events &&
+            <EventEmitterListener event="reloadModel" events={args.events} onCalled={reloadModel} />
+          }
+          {model && args.eventUpdated &&
+            <EventUpdated model={model} onUpdated={onUpdated} />
+          }
+          <WrappedComponent {...wrappedComponentProps} {...this.props} />
+        </>
+      )
+    }
+
+    reloadModel = () => this.loadModel()
+    onUpdated = this.loadExistingModel
   }
 
-  render() {
-    const {onUpdated, reloadModel} = digs(this, "onUpdated", "reloadModel")
-    const {model, modelId, notFound} = digs(this.state, "model", "modelId", "notFound")
-    const wrappedComponentProps = {}
+  if (args.loadByQueryParam) return withQueryParams(ModelLoadWrapper)
 
-    wrappedComponentProps[this.camelizedLower] = model
-    wrappedComponentProps[`${this.camelizedLower}Id`] = modelId
-    wrappedComponentProps[`${this.camelizedLower}NotFound`] = notFound
-
-    return (
-      <>
-        {args.events &&
-          <EventEmitterListener event="reloadModel" events={args.events} onCalled={reloadModel} />
-        }
-        {model && args.eventUpdated &&
-          <EventUpdated model={model} onUpdated={onUpdated} />
-        }
-        <WrappedComponent {...wrappedComponentProps} {...this.props} />
-      </>
-    )
-  }
-
-  reloadModel = () => this.loadModel()
-  onUpdated = this.loadExistingModel
+  return ModelLoadWrapper
 }
