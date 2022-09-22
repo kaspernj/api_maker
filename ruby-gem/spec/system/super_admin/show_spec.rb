9ae1e7549fc82a175cbc98b5ad56b17a49a5fe87
kaspernj <k@spernj.org>
2022-09-22 07:53:31 +0000

    Specced showing belongs-to-relationships in super admin

--- ruby-gem/spec/system/super_admin/show_spec.rb
+++ ruby-gem/spec/system/super_admin/show_spec.rb
@@ -1,7 +1,8 @@
 require "rails_helper"
 
 describe "super admin - show" do
-  let(:task) { create :task }
+  let(:project) { create :project, name: "Test project" }
+  let(:task) { create :task, project: project }
   let(:user_admin) { create :user, admin: true }
 
   it "renders the page" do
@@ -9,5 +10,8 @@ describe "super admin - show" do
     visit super_admin_path(model: "Task", model_id: task.id)
     wait_for_selector ".super-admin--show-page"
     wait_for_attribute_row attribute: "id", value: task.id
+
+    # It shows 'belongs_to'-relationships as attributes
+    wait_for_selector ".attribute-row-value", exact_text: "Test project"
   end
 end
