c031ae7b1548b46fa7a228ea8d76536aa4eb20a8
kaspernj <k@spernj.org>
2022-10-24 10:02:25 +0000

    Added translatedAttributes helper

---
+++ npm-api-maker/src/translated-attributes.js
@@ -0,0 +1,11 @@
+export default function apiMakerTranslatedAttributes(attributeNames, availableLocales) {
+  translatedAttributes = []
+
+  for (const attribute of attributeNames) {
+    for (const locale of availableLocales) {
+      translatedAttributes.push(`${attribute}${locale.substring(0, 1).toUpperCase()}${locale.substring(1, 99)}`)
+    }
+  }
+
+  return translatedAttributes
+}
