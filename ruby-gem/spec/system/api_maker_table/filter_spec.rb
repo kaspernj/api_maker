90896c97aa2b0eaa0c465dda4fc6e48181461ea6
kaspernj <k@spernj.org>
2022-11-01 16:39:21 +0000

    Table: Fixed filtering on multi part name relationship

--- ruby-gem/spec/system/api_maker_table/filter_spec.rb
+++ ruby-gem/spec/system/api_maker_table/filter_spec.rb
@@ -1,10 +1,15 @@
 require "rails_helper"
 
 describe "bootstrap - live table - filter" do
+  let(:account1) { create :account, customer: customer1, name: "Account 1" }
+  let(:customer1) { create :customer, name: "Customer 1" }
   let(:task1) { create :task, created_at: "1985-06-17 10:30", name: "Task 1", project: project1 }
-  let(:project1) { create :project, name: "Project 1" }
+  let(:project1) { create :project, account: account1, name: "Project 1" }
+
+  let(:account2) { create :account, customer: customer2, name: "Account 2" }
+  let(:customer2) { create :customer, name: "Customer 2" }
   let(:task2) { create :task, created_at: "1989-03-18 14:00", name: "Task 2", project: project2 }
-  let(:project2) { create :project, name: "Project 2" }
+  let(:project2) { create :project, account: account2, name: "Project 2" }
   let(:user_admin) { create :user, admin: true }
 
   let(:filter_card_selector) { ".live-table--filter-card" }
@@ -53,6 +58,25 @@ describe "bootstrap - live table - filter" do
     wait_for_and_find(".attribute-element[data-model-class='Project'][data-attribute-name='name']").click
     wait_for_and_find(".predicate-select").select("eq")
     wait_for_and_find(".value-input").set("Project 2")
+    wait_for_and_find(".apply-filter-button").click
+    wait_for_no_selector model_row_selector(task1)
+    wait_for_selector model_row_selector(task2)
+  end
+
+  it "filters through a relationship with a name that has multiple parts" do
+    task1
+    task2
+
+    login_as user_admin
+    visit bootstrap_live_table_path
+    wait_for_selector model_row_selector(task1)
+    wait_for_selector model_row_selector(task2)
+    wait_for_and_find(".filter-button").click
+    wait_for_and_find(".add-new-filter-button").click
+    wait_for_and_find(".reflection-element[data-model-class='Task'][data-reflection-name='accountCustomer']").click
+    wait_for_and_find(".attribute-element[data-model-class='Customer'][data-attribute-name='name']").click
+    wait_for_and_find(".predicate-select").select("eq")
+    wait_for_and_find(".value-input").set("Customer 2")
     wait_for_and_find(".apply-filter-button").click
     wait_for_no_selector model_row_selector(task1)
     wait_for_selector model_row_selector(task2)
