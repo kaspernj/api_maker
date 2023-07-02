8cddf8cbdb676a1d7a9b939c80ec605efd7332a3
kaspernj <k@spernj.org>
2023-07-02 08:36:11 +0000

    Ransack 4 support

--- api_maker_table_rubygem/app/models/api_maker_table/application_record.rb
+++ api_maker_table_rubygem/app/models/api_maker_table/application_record.rb
@@ -1,5 +1,20 @@
 module ApiMakerTable
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
+
+    # Old version of method from Ransack 3
+    def self.ransackable_attributes(auth_object = nil)
+      @ransackable_attributes ||= if Ransack::SUPPORTS_ATTRIBUTE_ALIAS
+        column_names + _ransackers.keys + _ransack_aliases.keys +
+        attribute_aliases.keys
+      else
+        column_names + _ransackers.keys + _ransack_aliases.keys
+      end
+    end
+
+    # Old version of method from Ransack 3
+    def self.ransackable_associations(auth_object = nil)
+      @ransackable_associations ||= reflect_on_all_associations.map { |a| a.name.to_s }
+    end
   end
 end
