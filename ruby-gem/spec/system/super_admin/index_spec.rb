649f89843277953af4af09a4d250e3409328f72d
kaspernj <k@spernj.org>
2023-09-22 08:35:32 +0000

    Fixed bogus spec

--- ruby-gem/spec/system/super_admin/index_spec.rb
+++ ruby-gem/spec/system/super_admin/index_spec.rb
@@ -6,13 +6,17 @@ describe "super admin - index" do
   let(:user_admin) { create :user, admin: true }
 
   it "renders the page, then switches to the index page of another model" do
+    project
     task
 
     login_as user_admin
     visit super_admin_path(model: "Task")
     wait_for_selector model_row_selector(task)
 
+    # It goes to the projects page and renders the project rows
     wait_for_and_find(".components--admin--layout--menu--menu-item[data-identifier='Project']").click
+    wait_for_expect { expect(current_url).to end_with "/super_admin?model=Project" }
     wait_for_selector model_row_selector(project)
+    wait_for_selector model_row_selector(task.project)
   end
 end
