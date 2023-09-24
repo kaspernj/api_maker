dbaafca71456698b13b3a152cb05ad4ade631c29
kaspernj <k@spernj.org>
2023-09-24 12:08:25 +0000

    Clear up logs manually

--- ruby-gem/spec/system/models/create_spec.rb
+++ ruby-gem/spec/system/models/create_spec.rb
@@ -11,11 +11,11 @@ describe "model create" do
 
     wait_for_path models_create_path
 
-    wait_for_browser { find("[data-controller='models--create']", visible: false)["data-create-completed"] == "true" }
+    wait_for_browser { wait_for_and_find("[data-controller='models--create']", visible: false)["data-create-completed"] == "true" }
     wait_for_browser { Project.count > 0 }
 
-    created_project = Project.last
-    element = find("[data-controller='models--create']", visible: false)
+    created_project = Project.last!
+    element = wait_for_and_find("[data-controller='models--create']", visible: false)
 
     expect(created_project.name).to eq "test-create-project"
     expect(element["data-project-name"]).to eq "test-create-project"
@@ -31,5 +31,6 @@ describe "model create" do
 
     expect { visit_action.call }.to raise_error(RuntimeError, "UnhandledRejection: No access to create Project")
     sleep 1 # Some stuff in JS may be going on - give it some time to wrap up
+    browser_logs # Clear up the logs manually
   end
 end
