57d9bf6ffb80b373d5803da3571db20af1c8d43a
kaspernj <k@spernj.org>
2023-09-24 09:01:51 +0000

    Fixed expect_no_browser_errors

--- ruby-gem/lib/api_maker/spec_helper/expect_no_browser_errors.rb
+++ ruby-gem/lib/api_maker/spec_helper/expect_no_browser_errors.rb
@@ -1,18 +1,15 @@
 module ApiMaker::SpecHelper::ExpectNoBrowserErrors
   def expect_no_browser_errors
-    logs = browser_logs
-      .map(&:to_s)
-      .reject { |log| log.include?("Warning: Can't perform a React state update on an unmounted component.") }
-      .reject { |log| log.include?("DEBUG: ") }
-      .join("\n")
+    logs = browser_logs.keep_if { |log| log.level == "SEVERE" }
 
     expect_no_browser_window_errors
-    return if logs.blank? || logs.exclude?("SEVERE ")
 
-    # Lets try one more time - just in case browser window error got registered meanwhile
+    return if logs.empty?
+
+    # Lets try one more time - just in case browser window error is currently being recorded
     sleep 0.4
     expect_no_browser_window_errors
 
-    raise logs
+    raise logs.map(&:message).join("\n\n")
   end
 end
