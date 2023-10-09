7d9f742222abb8c8dad8b782f4fda4242c6a412f
kaspernj <k@spernj.org>
2023-10-09 12:50:16 +0000

    Added React import

--- npm-api-maker/src/resize-observer.jsx
+++ npm-api-maker/src/resize-observer.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types"
 import PropTypesExact from "prop-types-exact"
+import React from "react"
 
 export default class ApiMakerResizeObserver extends React.PureComponent {
   static propTypes = PropTypesExact({
