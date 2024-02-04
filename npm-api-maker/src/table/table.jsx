d6de7ed1427f7e4217549a08af60bb96dd8e4c21
kaspernj <k@spernj.org>
2024-02-04 13:25:25 +0000

    SortLink: Use defaultParams if no qParams are present

--- npm-api-maker/src/table/table.jsx
+++ npm-api-maker/src/table/table.jsx
@@ -537,6 +537,7 @@ class ApiMakerTable extends React.PureComponent {
   rowComponent = () => this.responsiveComponent("tr")
 
   headersContentFromColumns () {
+    const {defaultParams} = this.props
     const {preparedColumns, query} = digs(this.shape, "preparedColumns", "query")
     const ColumnInHeadComponent = this.columnInHeadComponent()
 
@@ -548,7 +549,7 @@ class ApiMakerTable extends React.PureComponent {
         {...this.columnProps(column)}
       >
         {tableSettingColumn.hasSortKey() && query &&
-          <SortLink attribute={tableSettingColumn.sortKey()} query={query} title={this.headerLabelForColumn(column)} />
+          <SortLink attribute={tableSettingColumn.sortKey()} defaultParams={defaultParams} query={query} title={this.headerLabelForColumn(column)} />
         }
         {(!tableSettingColumn.hasSortKey() || !query) &&
           this.headerLabelForColumn(column)
