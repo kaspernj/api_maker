908420a98352df60b6ae4190a6ac52041dce7d3e
kaspernj <k@spernj.org>
2024-02-24 14:21:09 +0000

    Started working on the useCollection hook

---
+++ npm-api-maker/src/use-collection.mjs
@@ -0,0 +1,266 @@
+import Collection from "./collection.mjs"
+import debounce from "debounce"
+import {digg} from "diggerize"
+import ModelEvents from "./model-events.mjs"
+import PropTypes from "prop-types"
+import PropTypesExact from "prop-types-exact"
+import {useCallback, useEffect} from "react"
+import useShape from "set-state-compare/src/use-shape.js"
+import useQueryParams from "on-location-changed/src/use-query-params.js"
+
+const useCollection = ({
+  abilities,
+  collection,
+  defaultParams,
+  groupBy = ["id"],
+  modelClass,
+  noRecordsAvailableContent = undefined,
+  noRecordsFoundContent = undefined,
+  onModelsLoaded,
+  pagination = false,
+  preloads = [],
+  queryMethod,
+  queryName,
+  select = {},
+  selectColumns
+}) => {
+  const s = useShape({
+    abilities,
+    collection,
+    defaultParams,
+    groupBy,
+    modelClass,
+    noRecordsAvailableContent,
+    noRecordsFoundContent,
+    onModelsLoaded,
+    pagination,
+    preloads,
+    queryMethod,
+    select,
+    selectColumns
+  })
+
+  if (!queryName) queryName = digg(modelClass.modelClassData(), "collectionKey")
+
+  const setModels = s.useState("models")
+  const setOverallCount = s.useState("overallCount")
+  const setQuery = s.useState("query")
+  const setQueryName = s.useState("queryName", queryName)
+  const setQueryPerKey = s.useState("queryPerKey", `${s.s.queryName}_per`)
+  const setQueryQName = s.useState("queryQName", `${s.s.queryName}_q`)
+  const setQuerySName = s.useState("querySName", `${s.s.queryName}_s`)
+  const setQueryPageName = s.useState("queryPageName", `${s.s.queryName}_page`)
+  const setQParams = s.useState("qParams")
+  const setResult = s.useState("result")
+  const setSearchParams = s.useState("searchParams")
+  const setShowNoRecordsAvailableContent = s.useState("showNoRecordsAvailableContent", false)
+  const setShowNoRecordsFoundContent = s.useState("showNoRecordsFoundContent", false)
+  const queryParams = useQueryParams()
+
+  s.updateMeta({queryParams})
+
+  useEffect(() => {
+    loadQParams()
+    loadModels()
+
+    if (s.p.noRecordsAvailableContent) loadOverallCount()
+  }, [])
+
+  useEffect(() => {
+    loadQParams()
+    loadModels()
+  }, [queryParams[s.s.queryQName], queryParams[s.s.queryPageName], queryParams[s.s.queryPerKey], queryParams[s.s.querySName], collection])
+
+  const loadOverallCount = useCallback(async () => {
+    const baseQuery = s.p.collection || s.p.modelClass.all()
+    const overallCount = await baseQuery.count()
+
+    setOverallCount(overallCount)
+    setShowNoRecordsAvailableContent(showNoRecordsAvailableContent({overallCount}))
+    setShowNoRecordsFoundContent(showNoRecordsFoundContent({overallCount}))
+  }, [])
+
+  const hasQParams = useCallback(() => {
+    if (s.s.queryQName in s.m.queryParams) return true
+
+    return false
+  }, [])
+
+  const qParams = useCallback(() => {
+    if (hasQParams()) return JSON.parse(digg(s.m.queryParams, s.s.queryQName))
+
+    return {}
+  }, [])
+
+  const loadQParams = useCallback(() => {
+    const qParams = hasQParams() ? qParams() : Object.assign({}, s.p.defaultParams)
+    const searchParams = []
+
+    if (s.m.queryParams[s.s.querySName]) {
+      for (const rawSearchParam of s.m.queryParams[s.s.querySName]) {
+        const parsedSearchParam = JSON.parse(rawSearchParam)
+
+        searchParams.push(parsedSearchParam)
+      }
+    }
+
+    setQParams(qParams)
+    setSearchParams(searchParams)
+  }, [])
+
+  const loadModels = useCallback(async () => {
+    let query = s.p.collection?.clone() || s.p.modelClass.ransack()
+
+    if (s.p.pagination) {
+      const page = s.m.queryParams[s.s.queryPageName] || 1
+      let per = s.m.queryParams[s.s.queryPerKey] || 30
+
+      if (per == "all") {
+        per = 999_999_999
+      } else {
+        per = Number(per)
+      }
+
+      query.page(page).per(per)
+    }
+
+    if (s.p.groupBy) query = query.groupBy(...s.p.groupBy)
+
+    query = query
+      .ransack(s.s.qParams)
+      .search(s.s.searchParams)
+      .searchKey(s.s.queryQName)
+      .pageKey(s.s.queryPageName)
+      .perKey(s.s.queryPerKey)
+      .preload(s.p.preloads)
+      .select(s.p.select)
+
+    if (s.p.abilities) query = query.abilities(s.p.abilities)
+    if (s.p.selectColumns) query = query.selectColumns(s.p.selectColumns)
+
+    let result
+
+    if (s.p.queryMethod) {
+      result = await s.p.queryMethod({query})
+    } else {
+      result = await query.result()
+    }
+
+    const models = result.models()
+
+    if (s.p.onModelsLoaded) {
+      s.p.onModelsLoaded({
+        models,
+        qParams: s.s.qParams,
+        query,
+        result
+      })
+    }
+
+    setQuery(query)
+    setResult(result)
+    setModels(result.models())
+    setShowNoRecordsAvailableContent(showNoRecordsAvailableContent({models}))
+    setShowNoRecordsFoundContent(showNoRecordsFoundContent({models}))
+  }, [])
+
+  const loadModelsDebounce = useCallback(debounce(loadModels), [])
+  const onModelCreated = loadModels
+
+  const onModelDestroyed = useCallback((args) => {
+    setModels(s.s.models.filter((model) => model.id() != args.model.id()))
+  }, [])
+
+  const onModelUpdated = useCallback((args) => {
+    const updatedModel = digg(args, "model")
+    const foundModel = s.s.models.find((model) => model.id() == updatedModel.id())
+
+    if (foundModel) loadModelsDebounce()
+  }, [])
+
+  const showNoRecordsAvailableContent = useCallback((args) => {
+    let models, overallCount
+
+    if (args.models !== undefined) {
+      models = args.models
+    } else if (s.s.models !== undefined) {
+      models = s.s.models
+    }
+
+    if (args.overallCount !== undefined) {
+      overallCount = args.overallCount
+    } else if (s.s.overallCount !== undefined) {
+      overallCount = s.s.overallCount
+    }
+
+    if (models === undefined || overallCount === undefined || s.p.noRecordsAvailableContent === undefined) return false
+    if (models.length === 0 && overallCount === 0 && s.p.noRecordsAvailableContent) return true
+  }, [])
+
+  const showNoRecordsFoundContent = useCallback((args) => {
+    let models, overallCount
+
+    if (args.models !== undefined) {
+      models = args.models
+    } else if (s.s.models !== undefined) {
+      models = s.s.models
+    }
+
+    if (args.overallCount !== undefined) {
+      overallCount = args.overallCount
+    } else if (s.s.overallCount !== undefined) {
+      overallCount = s.s.overallCount
+    }
+
+    if (models === undefined || s.p.noRecordsFoundContent === undefined) return false
+
+    // Dont show noRecordsAvailableContent together with noRecordsAvailableContent
+    if (models.length === 0 && overallCount === 0 && s.p.noRecordsAvailableContent) return false
+    if (models.length === 0 && s.p.noRecordsFoundContent) return true
+  }, [])
+
+  useEffect(() => {
+    const connectCreated = ModelEvents.connectCreated(s.p.modelClass, onModelCreated)
+
+    return () => {
+      connectCreated.unsubscribe()
+    }
+  }, [])
+
+  return {
+    models: s.s.models
+  }
+
+  /*
+    {s.s.models && s.s.models.map((model) =>
+      <React.Fragment key={model.id()}>
+        <EventDestroyed model={model} onDestroyed={onModelDestroyed} />
+        <EventUpdated model={model} onUpdated={onModelUpdated} />
+      </React.Fragment>
+    )}
+  */
+}
+
+useCollection.propTypes = PropTypesExact({
+  abilities: PropTypes.object,
+  appHistory: PropTypes.object,
+  className: PropTypes.string,
+  collection: PropTypes.instanceOf(Collection),
+  component: PropTypes.object,
+  defaultParams: PropTypes.object,
+  groupBy: PropTypes.array,
+  modelClass: PropTypes.func.isRequired,
+  noRecordsAvailableContent: PropTypes.func,
+  noRecordsFoundContent: PropTypes.func,
+  onModelsLoaded: PropTypes.func,
+  pagination: PropTypes.bool.isRequired,
+  paginateContent: PropTypes.func,
+  preloads: PropTypes.array.isRequired,
+  queryMethod: PropTypes.func,
+  queryName: PropTypes.string,
+  queryParams: PropTypes.object,
+  select: PropTypes.object,
+  selectColumns: PropTypes.object
+})
+
+export default useCollection
