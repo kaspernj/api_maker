4f5dfd6e1781896f54cbcc36ee802bea416c1bec
kaspernj <k@spernj.org>
2024-02-29 22:02:08 +0000

    Super admin: Fixed access issues

---
+++ ruby-gem/spec/system/super_admin/dashboard_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe "super admin - dashboard" do
+  let(:user_admin) { create :user, admin: true, email: "admin@example.com", first_name: "Admin", last_name: "Adminson" }
+
+  it "renders the page" do
+    login_as user_admin
+    visit super_admin_path
+
+    # It shows menu items
+    wait_for_selector ".components--admin--layout--menu--menu-item", exact_text: "Projects"
+    wait_for_selector ".components--admin--layout--menu--menu-item", exact_text: "Tasks"
+
+    # It shows the user who is signed in
+    wait_for_selector ".menu-user-name-container", exact_text: "Admin Adminson"
+  end
+end
