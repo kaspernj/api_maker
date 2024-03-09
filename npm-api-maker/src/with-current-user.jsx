cdc03cdfd7cc7941fb6b72699eb6e29e039ff829
kaspernj <k@spernj.org>
2024-03-09 09:27:42 +0000

    withCurrentUser: use useCurrentUser

--- npm-api-maker/src/with-current-user.jsx
+++ npm-api-maker/src/with-current-user.jsx
@@ -1,30 +1,9 @@
-import Devise from "./devise"
-import {digs} from "diggerize"
-import EventEmitterListener from "./event-emitter-listener"
-import PureComponent from "set-state-compare/src/pure-component"
-import React from "react"
+import useCurrentUser from "./use-current-user.mjs"
 
-export default (WrappedComponent) => class WithCurrentUser extends PureComponent {
-  state = {
-    currentUser: Devise.currentUser()
-  }
+export default (WrappedComponent) => (props) => {
+  const currentUser = useCurrentUser()
 
-  render() {
-    const {onDeviseSigned} = digs(this, "onDeviseSigned")
-    const {currentUser} = digs(this.state, "currentUser")
-
-    return (
-      <>
-        <EventEmitterListener events={Devise.events()} event="onDeviseSignIn" onCalled={onDeviseSigned} />
-        <EventEmitterListener events={Devise.events()} event="onDeviseSignOut" onCalled={onDeviseSigned} />
-        <WrappedComponent {...this.props} currentUser={currentUser} />
-      </>
-    )
-  }
-
-  onDeviseSigned = () => {
-    const currentUser = Devise.currentUser()
-
-    if (this.state.currentUser !== currentUser) this.setState({currentUser})
-  }
+  return (
+    <WrappedComponent {...props} currentUser={currentUser} />
+  )
 }
