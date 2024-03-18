2fa5b44f3219626347ea2ecc43ab506679799231
kaspernj <k@spernj.org>
2024-03-18 08:16:42 +0000

    Router functional refactor

--- npm-api-maker/src/router.jsx
+++ npm-api-maker/src/router.jsx
@@ -1,45 +1,38 @@
 import PropTypes from "prop-types"
-import React from "react"
-import shouldComponentUpdate from "set-state-compare/src/should-component-update"
+import React, {memo} from "react"
 import {Suspense} from "react"
 import withRouter from "./with-router"
 
-class ApiMakerRouter extends React.Component {
-  static propTypes = {
-    notFoundComponent: PropTypes.elementType,
-    requireComponent: PropTypes.func.isRequired
-  }
+const ApiMakerRouter = (props) => {
+  const {match, ...restProps} = props
+  const {matchingRoute} = match
 
-  shouldComponentUpdate(nextProps, nextState) {
-    return shouldComponentUpdate(this, nextProps, nextState)
-  }
+  if (!matchingRoute) {
+    if (props.notFoundComponent) {
+      const NotFoundComponent = props.notFoundComponent
 
-  render() {
-    const {match, ...restProps} = this.props
-    const {matchingRoute} = match
-
-    if (!matchingRoute) {
-      if (this.props.notFoundComponent) {
-        const NotFoundComponent = this.props.notFoundComponent
-
-        return (
-          <Suspense fallback={<div />}>
-            <NotFoundComponent match={match} />
-          </Suspense>
-        )
-      } else {
-        return null
-      }
+      return (
+        <Suspense fallback={<div />}>
+          <NotFoundComponent match={match} />
+        </Suspense>
+      )
+    } else {
+      return null
     }
-
-    const Component = this.props.requireComponent({routeDefinition: matchingRoute.parsedRouteDefinition.routeDefinition})
-
-    return (
-      <Suspense fallback={<div />}>
-        <Component match={match} {...restProps} />
-      </Suspense>
-    )
   }
+
+  const Component = props.requireComponent({routeDefinition: matchingRoute.parsedRouteDefinition.routeDefinition})
+
+  return (
+    <Suspense fallback={<div />}>
+      <Component match={match} {...restProps} />
+    </Suspense>
+  )
 }
 
-export default withRouter(ApiMakerRouter)
+ApiMakerRouter.propTypes = {
+  notFoundComponent: PropTypes.elementType,
+  requireComponent: PropTypes.func.isRequired
+}
+
+export default withRouter(memo(ApiMakerRouter))
