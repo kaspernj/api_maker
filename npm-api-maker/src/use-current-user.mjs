17822292c200c0268934715de014ba75bb2fad6c
kaspernj <k@spernj.org>
2024-03-21 08:15:26 +0000

    useCurrentUser: Load with request if not given beforehand through global vars

--- npm-api-maker/src/use-current-user.mjs
+++ npm-api-maker/src/use-current-user.mjs
@@ -1,10 +1,46 @@
-import {useCallback, useEffect, useState} from "react"
+import {useCallback, useEffect} from "react"
+import {camelize} from "inflection"
 import Devise from "./devise.mjs"
+import Services from "./services.mjs"
+import useShape from "set-state-compare/src/use-shape.js"
 
-const useCurrentUser = () => {
-  const [currentUser, setCurrentUser] = useState(Devise.currentUser())
+const currentUserData = {}
+
+const useCurrentUser = (args) => {
+  const s = useShape()
+
+  s.meta.scope = args?.scope || "user"
+  s.meta.scopeName = `current${camelize(s.m.scope)}`
+
+  const loadCurrentUserFromRequest = useCallback(async () => {
+    const result = await Services.current().sendRequest("Devise::Current", {scope: s.m.scope})
+    const current = digg(result, "current")
+
+    currentUserData[scope] = current
+
+    s.setStates[s.m.scopeName](current)
+  }, [])
+
+  const defaultCurrentUser = useCallback(() => {
+    if (s.m.scope in currentUserData) {
+      return currentUserData[scope]
+    }
+  }, [])
+
+  s.useStates({currentUser: defaultCurrentUser()})
+
   const updateCurrentUser = useCallback(() => {
-    setCurrentUser(Devise.currentUser())
+    s.set({currentUser: Devise[s.m.scopeName]()})
+  }, [])
+
+  useEffect(() => {
+    if (!(s.m.scope in currentUserData)) {
+      if (Devise.current().hasCurrentScope(s.m.scope)) {
+        currentUserData[s.m.scope] = Devise[s.m.scopeName]()
+      } else {
+        loadCurrentUserFromRequest()
+      }
+    }
   }, [])
 
   useEffect(() => {
@@ -13,7 +49,7 @@ const useCurrentUser = () => {
     return () => {
       Devise.events().removeListener("onDeviseSignIn", updateCurrentUser)
     }
-  })
+  }, [])
 
   useEffect(() => {
     Devise.events().addListener("onDeviseSignOut", updateCurrentUser)
@@ -21,9 +57,9 @@ const useCurrentUser = () => {
     return () => {
       Devise.events().removeListener("onDeviseSignOut", updateCurrentUser)
     }
-  })
+  }, [])
 
-  return currentUser
+  return s.s.currentUser
 }
 
 export default useCurrentUser
