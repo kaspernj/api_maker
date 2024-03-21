c95a81968ee12e1f211205758d5a063f2d8bacf6
kaspernj <k@spernj.org>
2024-03-21 09:25:13 +0000

    Never load from request if defined

--- npm-api-maker/src/use-current-user.mjs
+++ npm-api-maker/src/use-current-user.mjs
@@ -7,15 +7,28 @@ import useShape from "set-state-compare/src/use-shape.js"
 const currentUserData = {}
 
 const useCurrentUser = (args) => {
-  const s = useShape()
+  const s = useShape(args || {})
   const scope = args?.scope || "user"
   const scopeName = useMemo(() => `current${camelize(scope)}`, [scope])
 
   s.meta.scope = scope
   s.meta.scopeName = scopeName
 
+  const debugs = useCallback((debugCallback) => {
+    if (s.props.debug) {
+      let debugArgs = debugCallback()
+
+      if (!Array.isArray(debugArgs)) debugArgs = [debugArgs]
+
+      console.log("useCurrentUser", ...debugArgs)
+    }
+  })
+
   const loadCurrentUserFromRequest = useCallback(async () => {
     const {scope, scopeName} = s.m
+
+    debugs(() => `Loading ${scope} with request`)
+
     const result = await Services.current().sendRequest("Devise::Current", {scope})
     const current = digg(result, "current")
 
@@ -32,6 +45,8 @@ const useCurrentUser = (args) => {
     if (scope in currentUserData) {
       return currentUserData[scope]
     } else if (Devise.current().hasCurrentScope(scope)) {
+      debugs(() => `Setting ${scope} from current scope`)
+
       currentUserData[scope] = Devise[scopeName]()
 
       return currentUserData[scope]
@@ -53,7 +68,7 @@ const useCurrentUser = (args) => {
   }, [])
 
   useEffect(() => {
-    if (!(s.m.scope in currentUserData)) {
+    if (!(s.m.scope in currentUserData) && !Devise.current().hasCurrentScope(s.m.scope)) {
       loadCurrentUserFromRequest()
     }
   }, [])
