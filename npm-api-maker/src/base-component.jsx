329e9f89923a7f969565c7f6944e223c8893ef0b
kaspernj <k@spernj.org>
2024-06-24 10:32:37 +0000

    Fixed class name

--- npm-api-maker/src/base-component.jsx
+++ npm-api-maker/src/base-component.jsx
@@ -1,7 +1,7 @@
 import fetchingObject from "fetching-object"
 import {ShapeComponent} from "set-state-compare/src/shape-component"
 
-export default class ComponentsWooftechShapeComponent extends ShapeComponent {
+export default class BaseComponent extends ShapeComponent {
   p = fetchingObject(() => this.props)
   s = fetchingObject(() => this.shape || this.state)
   tt = fetchingObject(this)
