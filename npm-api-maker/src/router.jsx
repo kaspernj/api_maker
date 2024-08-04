8e8498416bb9f2524d8384e62e014aae616727c0
kaspernj <k@spernj.org>
2024-08-04 13:22:29 +0000

    Fixed router and simplified

--- npm-api-maker/src/router.jsx
+++ npm-api-maker/src/router.jsx
@@ -1,29 +1,45 @@
+import BaseComponent from "./base-component"
+import PropTypes from "prop-types"
+import propTypesExact from "prop-types-exact"
 import React, {memo} from "react"
-import withRouter from "./with-router"
+import {shapeComponent} from "set-state-compare/src/shape-component.js"
+import {Suspense} from "react"
+import useRouter from "./use-router"
 
-const ApiMakerRouter = memo((props) => {
-  const {match, ...restProps} = props
-  const {matchingRoute} = match
+export default memo(shapeComponent(class ApiMakerRouter extends BaseComponent {
+  static propTypes = propTypesExact({
+    notFoundComponent: PropTypes.elementType,
+    path: PropTypes.string,
+    requireComponent: PropTypes.func.isRequired,
+    routeDefinitions: PropTypes.array,
+    routes: PropTypes.array
+  })
 
-  if (!matchingRoute) {
-    if (props.notFoundComponent) {
-      const NotFoundComponent = props.notFoundComponent
+  render() {
+    const {notFoundComponent, path, requireComponent, routeDefinitions, routes} = this.props
+    const {match} = useRouter({path, routes, routeDefinitions})
+    const {matchingRoute} = match
 
-      return (
-        <Suspense fallback={<div />}>
-          <NotFoundComponent match={match} />
-        </Suspense>
-      )
-    } else {
-      return null
+    if (!matchingRoute) {
+      if (notFoundComponent) {
+        const NotFoundComponent = notFoundComponent
+
+        return (
+          <Suspense fallback={<div />}>
+            <NotFoundComponent match={match} />
+          </Suspense>
+        )
+      } else {
+        return null
+      }
     }
+
+    const Component = requireComponent({routeDefinition: matchingRoute.parsedRouteDefinition.routeDefinition})
+
+    return (
+      <Suspense fallback={<div />}>
+        <Component match={match} />
+      </Suspense>
+    )
   }
-
-  const Component = props.requireComponent({routeDefinition: matchingRoute.parsedRouteDefinition.routeDefinition})
-
-  return (
-    <Component match={match} {...restProps} />
-  )
-})
-
-export default withRouter(ApiMakerRouter)
+}))
