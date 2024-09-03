2e4c4c5428939c4404aedcd691948ae5ada40788
kaspernj <k@spernj.org>
2024-09-03 11:44:23 +0000

    Better error message

--- npm-api-maker/src/model-class-require.mjs
+++ npm-api-maker/src/model-class-require.mjs
@@ -1,10 +1,15 @@
-import {digg} from "diggerize"
 import * as inflection from "inflection"
 import * as models from "./models.mjs.erb"
 
 export default (modelName) => {
   const requireName = inflection.camelize(modelName)
-  const ModelClass = digg(models, requireName)
+  const ModelClass = models[requireName]
+
+  if (!ModelClass) {
+    const modelClasses = Object.keys(models).sort()
+
+    throw new Error(`No model called ${modelName} in ${modelClasses.join(", ")}`)
+  }
 
   return ModelClass
 }
