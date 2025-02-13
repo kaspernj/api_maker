cb41b91bdfad820d839b7b60c5f9ae2710b9f694
kaspernj <k@spernj.org>
2025-02-13 15:24:48 +0000

    useStyles and fixes

---
+++ npm-api-maker/src/use-styles.js
@@ -0,0 +1,50 @@
+import config from "./config"
+import {digg} from "diggerize"
+import * as inflection from "inflection"
+import useBreakpoint from "./use-breakpoint"
+import {useMemo} from "react"
+
+const useStyles = (styles, args, dependencies = []) => {
+  const breakpoint = useBreakpoint()
+  const breakpointName = digg(breakpoint, "name")
+  const actualDependencies = [...dependencies, breakpointName]
+
+  const listOfStyles = useMemo(() => {
+    const listOfStyles = []
+    const breakpointsReverse = [...config.getBreakPoints()].reverse()
+
+    for (const arg of args) {
+      if (typeof arg == "string") {
+        if (!(arg in styles)) {
+          throw new Error(`No such styling '${arg}' in given styles: ${Object.keys(styles).join(", ")}`)
+        }
+
+        listOfStyles.push(styles[arg])
+
+        for (const breakpointData of breakpointsReverse) {
+          const breakpointName = breakpointData[0]
+          const breakpointStyleNameUp = `${arg}${inflection.camelize(breakpointName)}Up`
+          const breakpointStyleNameDown = `${arg}${inflection.camelize(breakpointName)}Down`
+          const breakpointIsUp = digg(breakpoint, `${breakpointName}Up`)
+          const breakpointIsDown = digg(breakpoint, `${breakpointName}Down`)
+
+          if (breakpointStyleNameUp in styles && breakpointIsUp) {
+            listOfStyles.push(styles[breakpointStyleNameUp])
+          }
+
+          if (breakpointStyleNameDown in styles && breakpointIsDown) {
+            listOfStyles.push(styles[breakpointStyleNameDown])
+          }
+        }
+      } else {
+        throw new Error(`Unhandled type: ${typeof arg}`)
+      }
+    }
+
+    return listOfStyles
+  }, actualDependencies)
+
+  return listOfStyles
+}
+
+export default useStyles
--- npm-api-maker/src/utils/text.jsx
+++ npm-api-maker/src/utils/text.jsx
@@ -1,16 +1,38 @@
 import memo from "set-state-compare/src/memo"
-import React from "react"
+import React, {useMemo} from "react"
 import {Text} from "react-native"
 import {useDefaultStyle, WithDefaultStyle} from "./default-style"
 
 export default memo((props) => {
   const {style, ...restProps} = props
   const defaultStyle = useDefaultStyle()
-  const actualStyle = Object.assign({}, defaultStyle?.Text, style)
+  const {actualStyle, stylesList} = useMemo(() => {
+    const stylesList = []
+    const actualStyle = {}
+
+    if (defaultStyle?.Text) {
+      Object.assign(actualStyle, defaultStyle.Text)
+      stylesList.push(defaultStyle.Text)
+    }
+
+    if (style) {
+      if (Array.isArray(style)) {
+        for (const styleI of style) {
+          Object.assign(actualStyle, styleI)
+          stylesList.push(styleI)
+        }
+      } else {
+        Object.assign(actualStyle, style)
+        stylesList.push(style)
+      }
+    }
+
+    return {actualStyle, stylesList}
+  }, [defaultStyle?.Text, style])
 
   return (
     <WithDefaultStyle style={actualStyle}>
-      <Text style={actualStyle} {...restProps} />
+      <Text style={stylesList} {...restProps} />
     </WithDefaultStyle>
   )
 })
