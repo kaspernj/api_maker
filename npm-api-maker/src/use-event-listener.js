5d43b5fef7c65b1c6a5adc6b58606ca68a814ed0
kaspernj <kasper@diestoeckels.de>
2025-05-21 15:45:19 +0000

    Fixed issues with server side and Node support

--- npm-api-maker/src/use-event-listener.js
+++ npm-api-maker/src/use-event-listener.js
@@ -1,16 +1,21 @@
-import {useCallback, useLayoutEffect} from "react"
+import {useCallback, useEffect, useLayoutEffect} from "react"
+import useSSR from "use-ssr"
 
 const ApiMakerUseEventListener = (target, event, onCalled) => {
+  const {isServer} = useSSR()
+  const useWorkingEffect = isServer ? useEffect : useLayoutEffect
+
   const onCalledCallback = useCallback((...args) => {
     onCalled.apply(null, args)
   }, [target, event, onCalled])
 
-  useLayoutEffect(() => {
+  useWorkingEffect(() => {
     if (target) {
-      target.addEventListener(event, onCalledCallback)
+      const eventListener = target.addEventListener(event, onCalledCallback)
 
       return () => {
-        target.removeEventListener(event, onCalledCallback)
+        if (eventListener) eventListener.remove() // This is how its done in Expo + Jest.
+        if (target.removeEventListener) target.removeEventListener(event, onCalledCallback) // This is the "old" way in browsers.
       }
     }
   }, [target, event, onCalled])
