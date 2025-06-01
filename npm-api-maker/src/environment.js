02879ce055943e633b9f061add148783fc9b35ea
kaspernj <kasper@diestoeckels.de>
2025-06-01 08:08:11 +0000

    Detect environment

---
+++ npm-api-maker/src/environment.js
@@ -0,0 +1,13 @@
+let isBrowser = false
+let isNative = false
+let isServer = false
+
+if (navigator === undefined && navigator.product == "ReactNative") {
+  isNative = true
+} else if (window === undefined && window.document?.createElement) {
+  isBrowser = true
+} else {
+  isServer = true
+}
+
+export default {isBrowser, isNative, isServer}
