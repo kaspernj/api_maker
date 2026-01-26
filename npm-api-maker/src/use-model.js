2c4628f6f33ca36f47c36e4c3b5d9250cfa0875c
kaspernj <kasper@diestoeckels.de>
2026-01-26 15:06:51 +0000

    Fix useModel newIfNoId defaults

--- npm-api-maker/src/use-model.js
+++ npm-api-maker/src/use-model.js
@@ -1,5 +1,5 @@
 /* eslint-disable sort-imports */
-import {useCallback, useLayoutEffect, useMemo} from "react"
+import {useCallback, useLayoutEffect} from "react"
 import Devise from "./devise.js"
 import * as inflection from "inflection"
 import ModelEvents from "./model-events.js"
@@ -21,6 +21,7 @@ import useShape from "set-state-compare/build/use-shape.js"
  * @param {Function|object} modelClassArg
  * @param {object | ((args: {modelClass: typeof import("./base-model.js").default}) => useModelArgs)} [argsArg]
  */
+// eslint-disable-next-line complexity
 const useModel = (modelClassArg, argsArg = {}) => {
   const queryParams = useQueryParams()
   let args, modelClass
@@ -135,8 +136,27 @@ const useModel = (modelClassArg, argsArg = {}) => {
   }
 
   s.updateMeta({args, modelId, modelVariableName, queryParams})
+  if (s.meta.syncNewModel == undefined) s.meta.syncNewModel = false
 
-  useMemo(
+  if (s.m.active && s.props.newIfNoId && !s.m.modelId && !s.s.model && !s.m.syncNewModel) {
+    const defaultsResult = s.props.newIfNoId?.defaults?.()
+
+    if (!(defaultsResult && typeof defaultsResult.then == "function")) {
+      const ModelClass = modelClass
+      const paramKey = ModelClass.modelName().paramKey()
+      const modelDataFromParams = s.m.queryParams[paramKey] || {}
+      const modelData = Object.assign(defaultsResult || {}, s.props.newAttributes, modelDataFromParams)
+      const model = new ModelClass({
+        isNewRecord: true,
+        data: {a: modelData}
+      })
+
+      s.meta.syncNewModel = model
+      s.set({model})
+    }
+  }
+
+  useLayoutEffect(
     () => { loadModel() },
     cacheArgs
   )
