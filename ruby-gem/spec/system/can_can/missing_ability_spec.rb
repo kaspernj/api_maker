105b1ca251e386e16bc714fe2d2eed08794a0962
kaspernj <kasper@diestoeckels.de>
2026-02-05 19:26:52 +0000

    Use admin for missing ability spec

--- ruby-gem/spec/system/can_can/missing_ability_spec.rb
+++ ruby-gem/spec/system/can_can/missing_ability_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
 
 describe "can can - missing ability" do
-  let(:user) { create :user }
+  let(:admin_user) { create :user, :admin }
 
   it "auto-loads a missing ability" do
-    login_as user
+    login_as admin_user
     visit can_can_missing_ability_path
 
     wait_for_selector ".can-can-missing-ability[data-status='loading']"
