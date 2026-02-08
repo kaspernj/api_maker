425268a1b12f738513740d95bfef5fc20a094e1f
kaspernj <kasper@diestoeckels.de>
2026-02-08 11:39:30 +0000

    Define GlobalRequestDataTest at top level

--- ruby-gem/spec/dummy/app/services/global_request_data_test.rb
+++ ruby-gem/spec/dummy/app/services/global_request_data_test.rb
@@ -1,10 +1,8 @@
-module Services
-  class GlobalRequestDataTest < ApiMaker::BaseService
-    def perform
-      global_data = controller&.params&.fetch(:global, {}) || {}
-      global_data = global_data.to_unsafe_h if global_data.respond_to?(:to_unsafe_h)
+class GlobalRequestDataTest < ApiMaker::BaseService
+  def perform
+    global_data = controller&.params&.fetch(:global, {}) || {}
+    global_data = global_data.to_unsafe_h if global_data.respond_to?(:to_unsafe_h)
 
-      succeed!(global: global_data)
-    end
+    succeed!(global: global_data)
   end
 end
