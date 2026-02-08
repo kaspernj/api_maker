06830eec7536b488d9d91aea852f2ee6db93ad8a
kaspernj <kasper@diestoeckels.de>
2026-02-08 08:59:56 +0000

    Fix global request data test service constant

--- ruby-gem/spec/dummy/app/services/global_request_data_test.rb
+++ ruby-gem/spec/dummy/app/services/global_request_data_test.rb
@@ -1,8 +1,10 @@
-class Services::GlobalRequestDataTest < ApiMaker::BaseService
-  def perform
-    global_data = controller&.params&.fetch(:global, {}) || {}
-    global_data = global_data.to_unsafe_h if global_data.respond_to?(:to_unsafe_h)
+module Services
+  class GlobalRequestDataTest < ApiMaker::BaseService
+    def perform
+      global_data = controller&.params&.fetch(:global, {}) || {}
+      global_data = global_data.to_unsafe_h if global_data.respond_to?(:to_unsafe_h)
 
-    succeed!(global: global_data)
+      succeed!(global: global_data)
+    end
   end
 end
