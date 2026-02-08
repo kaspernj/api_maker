59a6b2d5030d7ba86a0f78203a6706b089995cd9
kaspernj <kasper@diestoeckels.de>
2026-02-08 11:43:04 +0000

    Namespace global request data test service

--- ruby-gem/spec/dummy/app/services/services/global_request_data_test.rb
+++ ruby-gem/spec/dummy/app/services/services/global_request_data_test.rb
@@ -1,4 +1,4 @@
-class GlobalRequestDataTest < ApiMaker::BaseService
+class Services::GlobalRequestDataTest < ApiMaker::BaseService
   def perform
     global_data = controller&.params&.fetch(:global, {}) || {}
     global_data = global_data.to_unsafe_h if global_data.respond_to?(:to_unsafe_h)
