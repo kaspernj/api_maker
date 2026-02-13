21ed8e49fe0e89ab8d03204dc51a949b991b97d2
kaspernj <kasper@diestoeckels.de>
2026-02-13 10:02:30 +0000

    Use extensionless form import

--- npm-api-maker/src/use-input.js
+++ npm-api-maker/src/use-input.js
@@ -6,7 +6,7 @@ import nameForComponent from "./inputs/name-for-component.js"
 import strftime from "strftime"
 import useShape from "set-state-compare/build/use-shape.js"
 import useValidationErrors from "./use-validation-errors.js"
-import {useForm} from "./form.js"
+import {useForm} from "./form"
 
 /**
  * @param {object} args
