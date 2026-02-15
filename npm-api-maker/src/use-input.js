73ab2a77a8048dc5818846d797130eed642496ff
kaspernj <kasper@diestoeckels.de>
2026-02-15 12:18:44 +0000

    Fix use-input import order and spacing for lint

--- npm-api-maker/src/use-input.js
+++ npm-api-maker/src/use-input.js
@@ -1,14 +1,12 @@
 /* eslint-disable implicit-arrow-linebreak, no-extra-parens, no-use-before-define, prefer-object-spread */
+import {dig, digg} from "diggerize"
+import {useCallback, useEffect, useMemo, useRef} from "react"
 import idForComponent from "./inputs/id-for-component.js"
 import nameForComponent from "./inputs/name-for-component.js"
 import strftime from "strftime"
+import {useForm} from "./form"
 import useShape from "set-state-compare/build/use-shape.js"
 import useValidationErrors from "./use-validation-errors.js"
-import {dig, digg} from "diggerize"
-import {useCallback, useEffect, useMemo, useRef} from "react"
-import {useForm} from "./form"
-
-
 
 /**
  * @param {object} args
