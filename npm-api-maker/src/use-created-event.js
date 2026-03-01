fa62a0646429fd55aec9b128f051aca1b08096a6
kaspernj <kasper@diestoeckels.de>
2026-03-01 07:21:26 +0000

    Use ShapeHook in model class event hooks

--- npm-api-maker/src/use-created-event.js
+++ npm-api-maker/src/use-created-event.js
@@ -1,80 +1,4 @@
-import {ShapeHook, useShapeHook} from "set-state-compare"
-import {useEffect} from "react"
-import ModelEvents from "./model-events.js" // eslint-disable-line sort-imports
-import PropTypes from "prop-types"
-import debounceFunction from "debounce"
-import propTypesExact from "prop-types-exact"
-
-/** Hook state container for created-event subscriptions. */
-class UseCreatedEventShapeHook extends ShapeHook {
-  static defaultProps = {
-    active: true,
-    debounce: 0,
-    onConnected: undefined
-  }
-
-  static propTypes = propTypesExact({
-    active: PropTypes.bool.isRequired,
-    debounce: PropTypes.oneOfType([
-      PropTypes.bool,
-      PropTypes.number
-    ]),
-    modelClass: PropTypes.func.isRequired,
-    onConnected: PropTypes.func,
-    onCreated: PropTypes.func.isRequired
-  })
-
-  /** @returns {Function} */
-  debouncedOnCreated() {
-    return this.cache(
-      "debouncedOnCreated",
-      () => {
-        if (typeof this.p.debounce == "number") {
-          return debounceFunction(this.p.onCreated, this.p.debounce)
-        }
-
-        return debounceFunction(this.p.onCreated)
-      },
-      [this.p.debounce, this.p.onCreated]
-    )
-  }
-
-  /** @param {any[]} callbackArgs */
-  onCreatedCallback(...callbackArgs) {
-    if (!this.p.active) {
-      return
-    }
-
-    if (this.p.debounce) {
-      this.debouncedOnCreated()(...callbackArgs)
-    } else {
-      this.p.onCreated(...callbackArgs)
-    }
-  }
-
-  /** @returns {void} */
-  setup() {
-    useEffect(() => {
-      const modelClassConnection = ModelEvents.connectModelClass(
-        this.p.modelClass,
-        "creates",
-        (...callbackArgs) => this.onCreatedCallback(...callbackArgs)
-      )
-
-      if (this.p.onConnected) {
-        modelClassConnection.events.addListener("connected", this.p.onConnected)
-      }
-
-      return () => {
-        if (this.p.onConnected) {
-          modelClassConnection.events.removeListener("connected", this.p.onConnected)
-        }
-
-        modelClassConnection.unsubscribe()
-      }
-    }, [])
-  }
-}
+import useModelClassEvent from "./use-model-class-event.js"
 
 /**
  * @param {Function} modelClass
@@ -90,13 +14,7 @@ const ApiMakerUseCreatedEvent = (
   onCreated,
   args = {active: true, debounce: 0, onConnected: undefined}
 ) => {
-  const {active = true, debounce = 0, onConnected, ...restProps} = args
-
-  if (Object.keys(restProps).length > 0) {
-    throw new Error(`Unknown props given to ApiMakerUseCreatedEvent: ${Object.keys(restProps).join(", ")}`)
-  }
-
-  useShapeHook(UseCreatedEventShapeHook, {active, debounce, modelClass, onConnected, onCreated})
+  useModelClassEvent(modelClass, "creates", onCreated, args)
 }
 
 export default ApiMakerUseCreatedEvent
--- npm-api-maker/src/use-model-class-event.js
+++ npm-api-maker/src/use-model-class-event.js
@@ -1,8 +1,81 @@
-/* eslint-disable sort-imports */
+import {ShapeHook, useShapeHook} from "set-state-compare"
+import {useEffect} from "react"
+import ModelEvents from "./model-events.js" // eslint-disable-line sort-imports
+import PropTypes from "prop-types"
 import debounceFunction from "debounce"
-import ModelEvents from "./model-events.js"
-import {useCallback, useLayoutEffect} from "react"
-import useShape from "set-state-compare/build/use-shape.js"
+import propTypesExact from "prop-types-exact"
+
+/** Hook state container for model-class event subscriptions. */
+class UseModelClassEventShapeHook extends ShapeHook {
+  static defaultProps = {
+    active: true,
+    debounce: 0,
+    onConnected: undefined
+  }
+
+  static propTypes = propTypesExact({
+    active: PropTypes.bool.isRequired,
+    debounce: PropTypes.oneOfType([
+      PropTypes.bool,
+      PropTypes.number
+    ]),
+    event: PropTypes.string.isRequired,
+    modelClass: PropTypes.func.isRequired,
+    onCallback: PropTypes.func.isRequired,
+    onConnected: PropTypes.func
+  })
+
+  /** @returns {Function} */
+  debouncedOnCallback() {
+    return this.cache(
+      "debouncedOnCallback",
+      () => {
+        if (typeof this.p.debounce == "number") {
+          return debounceFunction(this.p.onCallback, this.p.debounce)
+        }
+
+        return debounceFunction(this.p.onCallback)
+      },
+      [this.p.debounce, this.p.onCallback]
+    )
+  }
+
+  /** @param {any[]} callbackArgs */
+  onModelClassEventCallback(...callbackArgs) {
+    if (!this.p.active) {
+      return
+    }
+
+    if (this.p.debounce) {
+      this.debouncedOnCallback()(...callbackArgs)
+    } else {
+      this.p.onCallback(...callbackArgs)
+    }
+  }
+
+  /** @returns {void} */
+  setup() {
+    useEffect(() => {
+      const modelClassConnection = ModelEvents.connectModelClass(
+        this.p.modelClass,
+        this.p.event,
+        (...callbackArgs) => this.onModelClassEventCallback(...callbackArgs)
+      )
+
+      if (this.p.onConnected) {
+        modelClassConnection.events.addListener("connected", this.p.onConnected)
+      }
+
+      return () => {
+        if (this.p.onConnected) {
+          modelClassConnection.events.removeListener("connected", this.p.onConnected)
+        }
+
+        modelClassConnection.unsubscribe()
+      }
+    }, [])
+  }
+}
 
 /**
  * @param {Function} modelClass
@@ -16,56 +89,13 @@ import useShape from "set-state-compare/build/use-shape.js"
  */
 // eslint-disable-next-line max-params
 const apiMakerUseModelClassEvent = (modelClass, event, onCallback, args = {}) => {
-  const {active = true, debounce, onConnected, ...restProps} = args
-  const s = useShape({active, debounce, event, modelClass, onCallback})
+  const {active = true, debounce = 0, onConnected, ...restProps} = args
 
   if (Object.keys(restProps).length > 0) {
     throw new Error(`Unknown props given to apiMakerUseModelClassEvent: ${Object.keys(restProps).join(", ")}`)
   }
 
-  const eventDebounce = useCallback(() => {
-    if (!s.meta.debounceInstance) {
-      if (typeof s.props.debounce == "number") {
-        s.meta.debounceInstance = debounceFunction(s.p.onCallback, s.p.debounce)
-      } else {
-        s.meta.debounceInstance = debounceFunction(s.p.onCallback)
-      }
-    }
-
-    return s.meta.debounceInstance
-  }, [])
-
-  const onModelClassEventCallback = useCallback((...callbackArgs) => {
-    if (!s.p.active) {
-      return
-    }
-
-    if (s.p.debounce) {
-      eventDebounce()(...callbackArgs)
-    } else {
-      s.p.onCallback(...callbackArgs)
-    }
-  }, [])
-
-  useLayoutEffect(() => {
-    const modelClassConnection = ModelEvents.connectModelClass(
-      s.p.modelClass,
-      s.p.event,
-      (...callbackArgs) => onModelClassEventCallback(...callbackArgs)
-    )
-
-    if (onConnected) {
-      modelClassConnection.events.addListener("connected", onConnected)
-    }
-
-    return () => {
-      if (onConnected) {
-        modelClassConnection.events.removeListener("connected", onConnected)
-      }
-
-      modelClassConnection.unsubscribe()
-    }
-  }, [])
+  useShapeHook(UseModelClassEventShapeHook, {active, debounce, event, modelClass, onCallback, onConnected})
 }
 
 export default apiMakerUseModelClassEvent
