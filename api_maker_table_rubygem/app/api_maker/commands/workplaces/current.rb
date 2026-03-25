9361f4ffaac4f49fd32854cf9b233fea3932954e
kaspernj <kasper@diestoeckels.de>
2026-03-25 10:04:22 +0000

    Add NameError debugging to Workplaces::Current command

--- api_maker_table_rubygem/app/api_maker/commands/workplaces/current.rb
+++ api_maker_table_rubygem/app/api_maker/commands/workplaces/current.rb
@@ -1,6 +1,7 @@
 class Commands::Workplaces::Current < Commands::ApplicationCommand
   def execute!
-    workplace_query = WorkerPlugins::Workplace.where(id: current_workplace)
+    workplace = current_workplace
+    workplace_query = WorkerPlugins::Workplace.where(id: workplace)
 
     # Support passing params to also load abilities and avoid doing another commands for this in the bottom bar
     collection_serializer = ApiMaker::CollectionSerializer.new(
@@ -13,7 +14,7 @@ class Commands::Workplaces::Current < Commands::ApplicationCommand
 
     # Support to count links to avoid doing another commands in the bottom bar
     if args&.dig(:links_count)
-      response[:links_count] = current_workplace
+      response[:links_count] = workplace
         .workplace_links
         .ransack(args.dig(:links_count, :ransack))
         .result
@@ -21,5 +22,11 @@ class Commands::Workplaces::Current < Commands::ApplicationCommand
     end
 
     succeed!(response)
+  rescue NameError => e
+    Rails.logger.error "[Workplaces::Current] NameError: #{e.message}"
+    Rails.logger.error "[Workplaces::Current] Workplace columns: #{WorkerPlugins::Workplace.column_names.inspect}"
+    Rails.logger.error "[Workplaces::Current] Resource attributes: #{Resources::WorkplaceResource._attributes.keys.inspect}"
+    Rails.logger.error "[Workplaces::Current] Backtrace:\n#{e.backtrace.first(20).join("\n")}"
+    raise
   end
 end
